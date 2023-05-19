d957e2242ae5480360edac955e299a9205f1d1c0
Artur Rodrigues <artur190503@outlook.com>
2023-05-19 01:46:58 -0300

    Tela de Cadastro Header > > Co-authored-by: Miguel de Frias <miguelfrias8@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import IconGov from './assets/govIcon.png'
+import ButtonContraste from './assets/buttonContraste.png'
+import LogoDNIT from './assets/logoDnit.png'
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="HeaderIcon">
+          <img className="iconGov" src={IconGov} alt="ícone gov" />
+          <p>DNIT</p>
+        </div>
+        <div className="HeaderContraste">
+          <img className= "buttonContraste"src={ButtonContraste} alt="Button contraste" />
+          <p>Alto contraste</p>
+        </div>
       </header>
+      <div className='Secao'>
+        <div className='Lateral'>
+          <img className= "logoDnit"src={LogoDNIT} alt="Logo DNIT" />
+        </div>
+        <div className='Central'></div>
+      </div>
     </div>
   );
 }
