b8b08228c0faa2557d46cbd918515d7fbc98d7ba
Artur Rodrigues <artur190503@outlook.com>
2023-05-21 19:11:23 -0300

    Atualização cadastro
    Co-authored-by: Miguel de Frias <miguelfrias8@gmail.com>

--- src/components/FormularioCadastro.tsx
+++ src/components/FormularioCadastro.tsx
@@ -6,12 +6,10 @@ import React from 'react';
 
 const formItemLayout = {
   labelCol: {
-    xs: { span: 24 },
-    sm: { span: 8 },
+    span: 24,
   },
   wrapperCol: {
-    xs: { span: 24 },
-    sm: { span: 16 },
+    span: 24,
   },
 };
 
@@ -25,6 +23,7 @@ const CadastroForm: React.FC = () => {
 
   return (
     <Form
+      
       {...formItemLayout}
       form={form}
       name="register"
@@ -33,68 +32,77 @@ const CadastroForm: React.FC = () => {
       style={{ maxWidth: 600 }}
       scrollToFirstError
     >
-      <Form.Item
-        name="email"
-        label="E-mail"
-        rules={[
-          {
-            type: 'email',
-            message: 'The input is not valid E-mail!',
-          },
-          {
-            required: true,
-            message: 'Please input your E-mail!',
-          },
-        ]}
-      >
+
+        <Form.Item 
+            name="nome"
+            label="Nome Completo"
+            tooltip="Seu nome completo!"
+            rules={[{ required: true, message: 'Por favor, coloque seu nome completo!', whitespace: true }]}
+        >
         <Input />
-      </Form.Item>
+        </Form.Item>
 
-      <Form.Item
-        name="password"
-        label="Password"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your password!',
-          },
-        ]}
-        hasFeedback
-      >
-        <Input.Password />
-      </Form.Item>
-
-      <Form.Item
-        name="confirm"
-        label="Confirm Password"
-        dependencies={['password']}
-        hasFeedback
-        rules={[
-          {
-            required: true,
-            message: 'Please confirm your password!',
-          },
-          ({ getFieldValue }) => ({
-            validator(_, value) {
-              if (!value || getFieldValue('password') === value) {
-                return Promise.resolve();
-              }
-              return Promise.reject(new Error('The two passwords that you entered do not match!'));
+        <Form.Item
+            name="email"
+            label="E-mail"
+            rules={[
+            {
+                type: 'email',
+                message: 'Entrada inválida!',
             },
-          }),
-        ]}
-      >
-        <Input.Password />
-      </Form.Item>
+            {
+                required: true,
+                message: 'Por favor, coloque o seu e-mail!',
+            },
+            ]}
+        >
+            <Input />
+        </Form.Item>
 
-      <Form.Item
-        name="nickname"
-        label="Nickname"
-        tooltip="What do you want others to call you?"
-        rules={[{ required: true, message: 'Please input your nickname!', whitespace: true }]}
-      >
+        <Form.Item
+            name="senha"
+            label="Senha"
+            rules={[
+            {
+                required: true,
+                message: 'Por favor, coloque a sua senha!',
+            },
+            ]}
+            hasFeedback
+        >
+        <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+            name="confirmar"
+            label="Confirmar Senha"
+            dependencies={['senha']}
+            hasFeedback
+            rules={[
+            {
+                required: true,
+                message: 'Por favor, confirme a senha!',
+            },
+            ({ getFieldValue }) => ({
+                validator(_, value) {
+                if (!value || getFieldValue('senha') === value) {
+                    return Promise.resolve();
+                }
+                return Promise.reject(new Error('As senhas não coincidem!'));
+                },
+            }),
+            ]}
+        >
+        <Input.Password />
+        </Form.Item>
+
+        <Form.Item 
+            name="uflotacao"
+            label="UF de Lotação"
+            rules={[{ required: true, message: 'Por favor, coloque sua UF de Lotação!', whitespace: true }]}
+        >
         <Input />
-      </Form.Item>
+        </Form.Item>
 
     </Form>
   );
