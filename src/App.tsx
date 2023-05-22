aa51f4c6c081a3493ddd58e5e37de078922810cd
isadoragalvaoss <isadoragalvaoss@gmail.com>
2023-05-22 12:29:08 -0300

    Atualizando estilos

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,18 @@
-import React from 'react';
-import './App.css';
-import LogoDNIT from './assets/logoDnit.png'
-import CadastroForm from './components/FormularioCadastro';
-import Header from './components/Header';
-
-
+import "./App.css";
+import imgLateral from "./assets/imgLateral.png";
+import CadastroForm from "./components/FormularioCadastro";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <div className='Secao'>
-        <div className='Lateral'>
-          <img className= "logoDnit"src={LogoDNIT} alt="Logo DNIT" />
+      <Header />
+      <div className="Secao">
+        <div className="Lateral">
+          <img className="imgLateral" src={imgLateral} alt="Logo DNIT" />
         </div>
-        <div className='Central'>
-          <CadastroForm/>
+        <div className="Central">
+          <CadastroForm />
         </div>
       </div>
     </div>
