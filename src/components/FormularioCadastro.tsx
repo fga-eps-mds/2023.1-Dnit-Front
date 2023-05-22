1a3a7cf69330bdbef70acbf34252d254c2b49aa6
isadoragalvaoss <isadoragalvaoss@gmail.com>
2023-05-22 12:27:35 -0300

    Atualizando formulário

--- src/components/FormularioCadastro.tsx
+++ src/components/FormularioCadastro.tsx
@@ -1,114 +1,48 @@
-import {
-  Form,
-  Input,
-} from 'antd';
-import React from 'react';
-
-const formItemLayout = {
-  labelCol: {
-    span: 24,
-  },
-  wrapperCol: {
-    span: 24,
-  },
-};
+import { Button, Form, Input, Space } from "antd";
+import React from "react";
+import "../App.css";
+import LogoDNIT from "../assets/logoDnitAzul.png";
 
 const CadastroForm: React.FC = () => {
   const [form] = Form.useForm();
 
   const onFinish = (values: any) => {
-    console.log('Received values of form: ', values);
+    console.log("Received values of form: ", values);
   };
 
-
   return (
-    <Form
-      
-      {...formItemLayout}
-      form={form}
-      name="register"
-      onFinish={onFinish}
-      initialValues={{ residence: ['zhejiang', 'hangzhou', 'xihu'], prefix: '86' }}
-      style={{ maxWidth: 600 }}
-      scrollToFirstError
-    >
-
-        <Form.Item 
-            name="nome"
-            label="Nome Completo"
-            tooltip="Seu nome completo!"
-            rules={[{ required: true, message: 'Por favor, coloque seu nome completo!', whitespace: true }]}
+    <div className="form">
+      <img className="logoDnit" src={LogoDNIT} alt="Logo DNIT" />
+      <div>
+        <h2>Login</h2>
+        <Form
+          form={form}
+          name="validateOnly"
+          layout="vertical"
+          autoComplete="off"
+          onFinish={onFinish}
         >
-        <Input />
-        </Form.Item>
-
-        <Form.Item
-            name="email"
-            label="E-mail"
-            rules={[
-            {
-                type: 'email',
-                message: 'Entrada inválida!',
-            },
-            {
-                required: true,
-                message: 'Por favor, coloque o seu e-mail!',
-            },
-            ]}
-        >
+          <Form.Item name="email" label="Email" rules={[{ required: true }]}>
             <Input />
-        </Form.Item>
-
-        <Form.Item
-            name="senha"
-            label="Senha"
-            rules={[
-            {
-                required: true,
-                message: 'Por favor, coloque a sua senha!',
-            },
-            ]}
-            hasFeedback
-        >
-        <Input.Password />
-        </Form.Item>
-
-        <Form.Item
-            name="confirmar"
-            label="Confirmar Senha"
-            dependencies={['senha']}
-            hasFeedback
-            rules={[
-            {
-                required: true,
-                message: 'Por favor, confirme a senha!',
-            },
-            ({ getFieldValue }) => ({
-                validator(_, value) {
-                if (!value || getFieldValue('senha') === value) {
-                    return Promise.resolve();
-                }
-                return Promise.reject(new Error('As senhas não coincidem!'));
-                },
-            }),
-            ]}
-        >
-        <Input.Password />
-        </Form.Item>
-
-        <Form.Item 
-            name="uflotacao"
-            label="UF de Lotação"
-            rules={[{ required: true, message: 'Por favor, coloque sua UF de Lotação!', whitespace: true }]}
-        >
-        <Input />
-        </Form.Item>
-
-    </Form>
+          </Form.Item>
+          <Form.Item name="password" label="Senha" rules={[{ required: true }]}>
+            <Input.Password />
+          </Form.Item>
+          <p>Esqueceu a senha?</p>
+          <Form.Item>
+            <Space>
+              <Button htmlType="reset" className="signUpButton">
+                <p>Cadastrar-se</p>
+              </Button>
+              <Button type="primary" htmlType="submit" className="loginButton">
+                <p>Entrar</p>
+              </Button>
+            </Space>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
   );
 };
 
 export default CadastroForm;
-
-
-
