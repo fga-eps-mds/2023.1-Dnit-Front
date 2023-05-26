4b7135bc939668cb37b44b91ddb04a4de88151ef
Yan Luca <yanlucaviana@gmail.com>
2023-05-26 16:49:35 -0300

    Colocando Radio Button na tela de Cadastro
    Co-authored-by: Miguel Matos Costa De Frias Barbosa <miguel.barbosa@aluno.unb.br>

--- src/components/form/FormularioCadastro.tsx
+++ src/components/form/FormularioCadastro.tsx
@@ -1,17 +1,24 @@
-import { Button, Form, Input, Space} from "antd";
+import { Form, Input, Space, Radio } from "antd";
 import React from "react";
-import "../../styles/App.css";
-
+import { Link } from "react-router-dom";
+import LogoDNIT from "../../assets/logoDnitAzul.png";
+import "../../styles/form.css";
+import ButtonComponent from "../Button";
 
 const CadastroForm: React.FC = () => {
   const [form] = Form.useForm();
-
+  const rules = [
+    {
+      required: true,
+      message: "Por favor, preencha o campo ${name}!",
+    },
+  ];
   const onFinish = (values: any) => {
     console.log("Received values of form: ", values);
   };
 
   return (
-    <div className="formCadastro">
+    <div className="form">
       <div>
         <h2>Cadastro</h2>
         <Form
@@ -20,62 +27,61 @@ const CadastroForm: React.FC = () => {
           layout="vertical"
           autoComplete="off"
           onFinish={onFinish}
+          requiredMark="optional"
+          className="form-email"
         >
-          <Form.Item name="Nome Completo" label="Nome Completo" rules={[{ required: true }]}>
-              <Input className="input-form" /> 
+          <Form.Item name="nome completo" label="Nome Completo" rules={rules}>
+            <Input
+              className="inputForm"
+              prefix={<i className="fas fa-user"></i>}
+            />
           </Form.Item>
-          <Form.Item name="Email Institucional" label="Email Institucional" rules={[{ required: true }]}>
-            <Input className="input-form"/>  
+
+          <Form.Item name="email" label="E-mail" rules={rules}>
+            <Input
+              prefix={<i className="fas fa-envelope"></i>}
+              className="inputForm"
+            />
           </Form.Item>
-          <Form.Item
-<<<<<<< HEAD:src/components/FormularioCadastro.tsx
-            name="Senha"
-            label="Senha"
-=======
-            name="confirmPassword"
-            label="Confirmar Senha"
->>>>>>> f9374d8ebf45d1002c8df6cfe99a0ad2cfd234a4:src/components/form/FormularioCadastro.tsx
-            rules={[{ required: true }]}
-          >
-            <Input.Password className="input-form" />
+          <Form.Item name="senha" label="Senha" rules={rules}>
+            <Input.Password
+              className="inputForm"
+              prefix={<i className="fas fa-lock"></i>}
+            />
           </Form.Item>
-          <Form.Item
-<<<<<<< HEAD:src/components/FormularioCadastro.tsx
-            name="senha"
-            label="Confirmar senha"
-            rules={[{ required: true }]}
-          >
-            <Input.Password className="input-form"/>
-=======
-            name="nome"
-            label="Nome Completo"
-            rules={[{ required: true }]}
-          >
-            <Input />
-          </Form.Item>
-          <Form.Item
-            name="uf"
-            label="UF de lotação"
-            rules={[{ required: true }]}
-          >
-            <Input />
->>>>>>> f9374d8ebf45d1002c8df6cfe99a0ad2cfd234a4:src/components/form/FormularioCadastro.tsx
+
+          <Form.Item name="confirmar senha" label="Confirmar Senha" rules={rules}>
+            <Input.Password
+              className="inputForm"
+              prefix={<i className="fas fa-lock"></i>}
+            />  
           </Form.Item>
 
           <Form.Item>
+            <Radio.Group  defaultValue={false}>
+              <Radio name="Usuário DNIT" value={true} >
+                <p className="radio1">Usuário DNIT</p>
+              </Radio>
+              <Radio defaultChecked value={false}>
+                <p className="radio2">Empresa Executora</p></Radio>
+            </Radio.Group>
+          </Form.Item>
+          
+          <Form.Item>
             <Space>
-<<<<<<< HEAD:src/components/FormularioCadastro.tsx
-              <Button  type="primary" htmlType="submit" className="signUpButtonC">
-=======
-              <Button htmlType="submit" className="signUpButton">
->>>>>>> f9374d8ebf45d1002c8df6cfe99a0ad2cfd234a4:src/components/form/FormularioCadastro.tsx
-                <p>Cadastrar-se</p>
-              </Button>
+              <ButtonComponent
+                nome="Cadastrar-se"
+                cor="#1351B4"
+                cor_letra="#FFFFFF"
+                cor_borda="#1351B4"
+                largura="25em"
+              />
             </Space>
           </Form.Item>
-          <Form.Item><a href="">Já possui cadastro? Faça o Login</a></Form.Item>
+
+          <Link to="/login">Já possui cadastro? Faça o Login</Link>
         </Form>
-        <a className="politica" href="">Política de privacidade</a><a href=""></a><a className="ajuda" href="">Precisa de Ajuda</a><a href=""></a>
+        <a href="" className="politica" > Política de privacidade </a> <a href="" className="ajuda"> Precisa de ajuda?</a>
       </div>
     </div>
   );
