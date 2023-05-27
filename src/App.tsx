ba4b4c1ee6bac9c4a4bd793697e9dd0d59277837
Carolina Barbosa <carolinabarbosab09@gmail.com>
2023-05-26 21:11:03 -0300

    #8 Inicio da implemementacao da tela esqueci minha senha
    Co-authored-by: JuliaSSouza <juliasantanna.souza@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Cadastro from "./pages/Cadastro";
 import Login from "./pages/Login";
+import EsqueciSenha from "./pages/EsqueciSenha";
 import "./styles/App.css";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/cadastro" element={<Cadastro />} />
+      <Route path="/esquecisenha" element={<EsqueciSenha />} />
     </Routes>
   );
 }
