1ca7d49bef5cf85f71a5992001feba2a0545921f
isadoragalvaoss <isadoragalvaoss@gmail.com>
2023-05-30 23:46:32 -0300

    Correção da rota da api

--- src/consts/service.ts
+++ src/consts/service.ts
@@ -1,4 +1,4 @@
-const urlAPI = "http://back-dnit-usuario-env.eba-y6czyufk.us-east-1.elasticbeanstalk.com/api";
+const urlAPI = "https://api.aprovaunb.com/api";
 
 const unidadeFederativaURL = `${urlAPI}/dominio/unidadeFederativa`;
 
