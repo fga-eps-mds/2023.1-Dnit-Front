9bb971fafc43e6c0156a051374c5234bc7e55133
isadoragalvaoss <isadoragalvaoss@gmail.com>
2023-05-31 23:37:40 -0300

    Adição do campo select UF -  [BUG] Campo de UF #23
    Antonio Neto <netocaastro10@gmail.com>

--- src/components/form/FormularioCadastro.tsx
+++ src/components/form/FormularioCadastro.tsx
@@ -1,17 +1,22 @@
 import ButtonComponent from "../Button";
 // import { faBullseye } from "@fortawesome/free-solid-svg-icons";
-import { Button, Form, Input, Space, notification, Radio } from "antd";
+import { Form, Input, Radio, Select, Space, notification } from "antd";
 // import { AuthContext } from "../../provider/Authentication";
 import fetchCadastro from "../../service/cadastro";
 // import LogoDNIT from "../../assets/logoDnitAzul.png"
 
-import { Link } from "react-router-dom";
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import fetchUnidadeFederativa from "../../service/unidadeFederativa";
 import "../../styles/form.css";
+const { Option } = Select;
 
+interface UfProps {
+  value: number;
+  label: string;
+}
 const CadastroForm: React.FC = () => {
   const [form] = Form.useForm();
-
 
   const [api, contextHolder] = notification.useNotification();
   const rules = [
@@ -24,6 +29,7 @@ const CadastroForm: React.FC = () => {
   const [visible, setVisible] = useState(false);
 
   const [visible2, setVisible2] = useState(false);
+  const [uf, setUf] = useState<UfProps[]>();
 
   const onFinish = async (values: any) => {
     console.log("Received values of form: ", values);
@@ -36,30 +42,40 @@ const CadastroForm: React.FC = () => {
 
     try {
       await fetchCadastro(cadastroData);
+      api.success({ message: "Cadastro feito!" });
       // login();
     } catch (error) {
       api.error({ message: `Erro ao fazer cadastro` });
     }
   };
-return (
-  <div className="formc">
-    <div>
-      <h2>Cadastro</h2>
-      <Form
-        form={form}
-        name="validateOnly"
-        layout="vertical"
-        autoComplete="off"
-        onFinish={onFinish}
-        requiredMark="optional"
-        className="form-email"
-      >
-        <Form.Item name="nome" label="Nome Completo" rules={rules}>
-          <Input
-            className="inputForm"
-            prefix={<i className="fas fa-user"></i>}
-          />
-        </Form.Item>
+
+  async function fetchUf() {
+    const uf = await fetchUnidadeFederativa();
+    const newuf = uf.map((u) => ({ value: u.id, label: u.descricao }));
+    setUf(newuf);
+  }
+
+  console.log(uf);
+  return (
+    <div className="formc">
+      {contextHolder}
+      <div>
+        <h2>Cadastro</h2>
+        <Form
+          form={form}
+          name="validateOnly"
+          layout="vertical"
+          autoComplete="off"
+          onFinish={onFinish}
+          requiredMark="optional"
+          className="form-email"
+        >
+          <Form.Item name="nome" label="Nome Completo" rules={rules}>
+            <Input
+              className="inputForm"
+              prefix={<i className="fas fa-user"></i>}
+            />
+          </Form.Item>
 
           <Form.Item name="email" label="E-mail" rules={rules}>
             <Input
@@ -67,7 +83,7 @@ return (
               className="inputForm"
             />
           </Form.Item>
-        
+
           <Form.Item name="senha" label="Senha" rules={rules}>
             <Input.Password
               className="inputForm"
@@ -75,7 +91,11 @@ return (
             />
           </Form.Item>
 
-          <Form.Item name="confirmar senha" label="Confirmar Senha" rules={rules}>
+          <Form.Item
+            name="confirmar senha"
+            label="Confirmar Senha"
+            rules={rules}
+          >
             <Input.Password
               className="inputForm"
               prefix={<i className="fas fa-lock"></i>}
@@ -83,37 +103,69 @@ return (
           </Form.Item>
 
           <Form.Item className="item" name="tipo de usuário" rules={rules}>
-            <Radio.Group className="radioButtons" name="tipo de usuário" >
-              <Radio value={"DNIT"} checked={visible} onClick={() => { setVisible(true); setVisible2(false) }} >
+            <Radio.Group className="radioButtons" name="tipo de usuário">
+              <Radio
+                value={"DNIT"}
+                checked={visible}
+                onClick={() => {
+                  setVisible(true);
+                  setVisible2(false);
+                }}
+              >
                 <p className="radio1">Usuário DNIT</p>
               </Radio>
-              <Radio value={"Terceirizada"} checked={visible2} onClick={() => { setVisible2(true); setVisible(false) }} >
+              <Radio
+                value={"Terceirizada"}
+                checked={visible2}
+                onClick={() => {
+                  setVisible2(true);
+                  setVisible(false);
+                }}
+              >
                 <p className="radio2">Empresa Terceirizada</p>
               </Radio>
             </Radio.Group>
           </Form.Item>
 
-          {visible &&
-            <div>
-              <Form.Item className="ext1" name="uf de lotação" label="UF de Lotação" rules={rules}>
-                <Input
-                  prefix={<i className="fas fa-city"></i>}
-                  className="inputForm"
-                />
+          {visible && (
+            <div className=" select-group">
+              <i className="fas fa-city" />
+              <Form.Item
+                className="ext1 "
+                name="uf de lotação"
+                label="UF de Lotação"
+                rules={rules}
+              >
+                <Select
+                  onClick={fetchUf}
+                  notFoundContent={<p>Carregando...</p>}
+                  placement="topLeft"
+                >
+                  {uf?.map((u) => (
+                    <Option key={u.value} value={u.value}>
+                      {u.label}
+                    </Option>
+                  ))}
+                </Select>
               </Form.Item>
             </div>
-          }
+          )}
 
-          {visible2 &&
+          {visible2 && (
             <div>
-              <Form.Item className="ext2" name="empresa executora" label="Empresa Executora" rules={rules}>
+              <Form.Item
+                className="ext2"
+                name="empresa executora"
+                label="Empresa Executora"
+                rules={rules}
+              >
                 <Input
                   prefix={<i className="fas fa-city"></i>}
                   className="inputForm"
                 />
               </Form.Item>
             </div>
-          }
+          )}
 
           <Form.Item>
             <Space>
@@ -129,11 +181,17 @@ return (
 
           <Link to="/login">Já possui cadastro? Faça o Login</Link>
         </Form>
-        <a href="" className="politica" > Política de privacidade </a> <a href="" className="ajuda"> Precisa de ajuda?</a>
+        <a href="" className="politica">
+          {" "}
+          Política de privacidade{" "}
+        </a>{" "}
+        <a href="" className="ajuda">
+          {" "}
+          Precisa de ajuda?
+        </a>
       </div>
     </div>
   );
 };
-
 
 export default CadastroForm;
