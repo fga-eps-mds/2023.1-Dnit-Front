5eb67b7c3bef74e5354e0afd676b0268a61d81f5
Miguel de Frias <miguelfrias8@gmail.com>
2023-06-04 17:51:59 -0300

    #15 Iniciando formulário multi-etapas

--- src/components/form/RegisterEscola.tsx
+++ src/components/form/RegisterEscola.tsx
@@ -1,18 +1,25 @@
 import "../../styles/form.css";
 import { Button, Form, Input, Radio, Select, Space, notification } from "antd";
 import ButtonComponent from "../Button";
+import { useState } from "react";
+import { link } from "fs";
+
 
 
 
 export default function RegS() {
+
+const [screen, setScreen] = useState<"form1"|"form2"|"form3">("form1");
+
 return(
     <div className="formrs">
-        <div>
+        {screen === "form1" && (
+            <div>
             <h2>Escolha como cadastrar</h2>
             <Form>
             <Form.Item className="insbutton" >
                 <Space>
-                <Button className="button1" type="primary" size="large" shape="round">
+                <Button className="button1" type="primary" size="large" shape="round" onClick={() => setScreen("form2")}>
                 Inserindo informações
                 </Button>
                 </Space>
@@ -26,11 +33,25 @@ return(
             </Form.Item>
 
           </Form>
-        </div>
+          </div>
+        )}
 
+        {screen === "form2" && (
+            <div>
+            <h2>Cadastrar Escola</h2>
+                    <Form.Item className="insbutton">
+                    <Space>
+                    <Button className="button1" type="primary" size="large" shape="round" onClick={() => setScreen("form1")}>
+                    Voltar
+                    </Button>
+                    </Space>
+                    </Form.Item>
+            </div>
+        )}
     </div>
 );
 }
+
 
 
 
