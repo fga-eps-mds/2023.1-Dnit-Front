a012030952696d68171f535a3c1893ceb25459bf
isadoragalvaoss <isadoragalvaoss@gmail.com>
2023-06-09 13:08:40 -0300

    Correção jest-environment-jsdom
    Co-authored-by: Antônio Neto <54555684+antoniotoineto@users.noreply.github.com>

--- jest.config.js
+++ jest.config.js
@@ -35,6 +35,6 @@ module.exports = {
   moduleNameMapper: {
     "^.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|css)$":
       "<rootDir>/__mocks__/fileMock.js",
-    axios: "axios/dist/node/axios.cjs",
+    "^axios$": require.resolve("axios"),
   },
 };
