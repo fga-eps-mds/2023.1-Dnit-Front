5136daf57972268a3376c67e7be6c45c3fe6b580
Carolina Barbosa <carolinabarbosab09@gmail.com>
2023-06-14 21:32:26 -0300

    #17 Adicionando novos inputs na modal
    Co-authored-by: GuilhermeSoaress <guilhermesoares278@gmail.com>

--- src/components/components-escolasCadastradas/ModalExibirInformacoes.tsx
+++ src/components/components-escolasCadastradas/ModalExibirInformacoes.tsx
@@ -2,36 +2,60 @@ import '../components-escolasCadastradas/ModalExibirInformacoes.css';
 
 export default function ModalExibirInformacoes() {
 
+    var x = "labael/rotulo";
     return (
-
         <div className="div br-modal large">
             <div className="br-modal-header">Nome da Escola
             </div>
             <div className="br-modal-body">
-                <div className="br-input input-inline">
-                    <div className="input-label">
+                <div className="br-input">
+                    <label htmlFor="input-default">{x}</label>
+                    <input id="input-default" type="text" placeholder="Placeholder" disabled/>
+                    <label htmlFor="input-default">{x}</label>
+                    <input id="input-default" type="text" placeholder="Placeholder" disabled/>
+                    <label htmlFor="input-default">{x}</label>
+                    <input id="input-default" type="text" placeholder="Placeholder" disabled/>
+                    <label htmlFor="input-default">{x}</label>
+                    <input id="input-default" type="text" placeholder="Placeholder" disabled/> 
+                    <label htmlFor="input-default">{x}</label>
+                    <input id="input-default" type="text" placeholder="Placeholder" disabled/> 
+                    <label htmlFor="input-default">{x}</label>
+                    <input id="input-default" type="text" placeholder="Placeholder" disabled/> 
+                    <label htmlFor="input-default">{x}</label>
+                    <input id="input-default" type="text" placeholder="Placeholder" disabled/>
+                    <label htmlFor="input-default">{x}</label>
+                    <input id="input-default" type="text" placeholder="Placeholder" disabled/>
+                    <label htmlFor="input-default">{x}</label>
+                    <input id="input-default" type="text" placeholder="Placeholder" disabled/>
+                    <label htmlFor="input-default">{x}</label>
+                    <input id="input-default" type="text" placeholder="Placeholder" disabled/>
+                    <label htmlFor="input-default">{x}</label>
+                    <input id="input-default" type="text" placeholder="Placeholder" disabled/>
+                    <label htmlFor="input-default">{x}</label>
+                    <input id="input-default" type="text" placeholder="Placeholder"/>
+                    <div className="br-select">
+                    <div className="br-input">
+                        <label htmlFor="select-simple">Label</label>
+                        <input id="select-simple" type="text" placeholder="Selecione o item"/>
+                        <button className="br-button" type="button" aria-label="Exibir lista"  data-trigger="data-trigger"><i className="fas fa-angle-down" aria-hidden="true"></i>
+                        </button>
                     </div>
-                    <div className="input-content">
-                        <div className="input-group">
-                            <div className="input-icon"><i className="fas fa-edit" aria-hidden="true"></i>
-                            </div>
-                            <input id="lateral-icon" type="text" placeholder="Placeholder" />
+                    <div className="br-list" >
+                        <div className="br-item" >
+                        <div className="br-radio">
+                            <input id="rb0" type="radio" name="estados-simples" value="rb0"/>
+                            <label htmlFor="rb0">Acre</label>
+                        </div>
+                        </div>
+                        <div className="br-item" >
+                        <div className="br-radio">
+                            <input id="rb1" type="radio" name="estados-simples" value="rb1"/>
+                            <label htmlFor="rb1">Alagoas</label>
+                        </div>
                         </div>
                     </div>
+                    
                 </div>
-
-                <div className="br-input">
-                    <label htmlFor="input-default">Label / Rótulo</label>
-                </div>
-                <div className="br-input">
-                    <label htmlFor="input-default">Label / Rótulo</label>
-                    <input id="input-default" type="text" placeholder="Placeholder" />
-                </div>
-                <div className="br-input">
-                    <label htmlFor="input-default">Label / Rótulo</label>
-                    <input id="input-default" type="text" placeholder="Placeholder" />
-                </div>
-
             </div>
             <div className="br-modal-footer justify-content-center">
                 <button className="br-button secondary" type="button">Negar
@@ -39,6 +63,7 @@ export default function ModalExibirInformacoes() {
                 <button className="br-button primary ml-2" type="button">Aceitar
                 </button>
             </div>
+        </div>
         </div>
     );
 }
