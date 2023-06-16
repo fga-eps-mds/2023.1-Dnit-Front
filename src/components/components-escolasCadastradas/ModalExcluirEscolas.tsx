f1fbfc59130c00ac8da3a63814071dd5cb1e2cdf
Letícia Resende <letigy78@gmail.com>
2023-06-16 14:15:15 -0300

    Implementação da modal confirmar exclusão
    Co-authored-by: Yan Luca <yanlucaviana@gmail.com>
    Co-authored-by: Felipe Agustini <felipeboccardi@hotmail.com>

--- src/components/components-escolasCadastradas/ModalExcluirEscolas.tsx
+++ src/components/components-escolasCadastradas/ModalExcluirEscolas.tsx
@@ -1,24 +1,23 @@
-import '../components-escolasCadastradas/ModalExcluirEscolas.css';
-import "../../styles/form.css";
-
+import './ModalExcluirEscolas.css';
 export default function ModalExibirInformacoes() {
 
     var x = "labael/rotulo";
     return (
         <div className="div br-modal large">
-            <div className="br-modal-header">Confirmar Exclusão
+            <div className="br-modal-header content-left">Confirmar Exclusão 
             </div>
             <div className='modal-line'>
                  
             </div>
-            <p>
+            
+            <p className='content-left space-p'>
                 Deseja excluir a escola permanentemende? A ação não pode ser desfeita.
             </p>
 
-            <div className="br-modal-footer justify-content-center">    
+            <div className="br-modal-footer content-right">    
                 <button className="br-button secondary" type="button">Voltar
                 </button>
-                <button className="br-button primary ml-2" type="button">Excluir
+                <button className="br-button cancel-button" type="button">Excluir
                 </button>
             </div>
         </div>
