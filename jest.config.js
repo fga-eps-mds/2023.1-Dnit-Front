2289b970833273a42f1107e3a1a8b6de42aef8bc
isadoragalvaoss <isadoragalvaoss@gmail.com>
2023-06-15 21:38:36 -0300

    Eliminando pasta de testes do coverage
    Co-authored-by: Antônio Neto <54555684+antoniotoineto@users.noreply.github.com>

--- jest.config.js
+++ jest.config.js
@@ -10,7 +10,7 @@ module.exports = {
     "!src/App.tsx",
     "!src/service/*.{ts,tsx}",
     "!src/provider/*.{ts,tsx}",
-    "!src/tests/*.{ts,tsx}",
+    "!src/tests/*",
   ],
   collectCoverage: true,
   testEnvironment: "jest-environment-jsdom",
