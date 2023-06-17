9712ac05d41c6b0c542ed96d38aab7fd44c8e91e
Julia Sant'Anna <juliasantanna.souza@gmail.com>
2023-06-17 15:24:25 -0300

    #26 ajustes na header
    Co-authored-by: Antonio Neto <netocaastro10@gmail.com>

--- src/components/components-escolasCadastradas/Header.tsx
+++ src/components/components-escolasCadastradas/Header.tsx
@@ -1,59 +1,53 @@
 import IconGov from "../../assets/govIcon.png";
 import ButtonContraste from "../../assets/icons/buttonContraste.png";
+import Header2 from "../../components/Header";
 
 import "../components-escolasCadastradas/Header.css";
 
 function Header() {
   return (
     <>
-<header className="br-header">
-  <div className="container-lg">
-    <div className="header-top">
-      <div className="header-logo"><img src={IconGov} alt="logo"/><span className="br-divider vertical"></span>
-        <div className="header-sign">DNIT</div>
-      </div>
-      <div className="header-actions">
-        <div className="header-links dropdown">
-          <button className="br-button circle small" type="button" data-toggle="dropdown" aria-label="Abrir Acesso Rápido"><i className="fas fa-ellipsis-v" aria-hidden="true"></i>
-          </button>
-          <div className="br-list">
-            <div className="header">
-              <div className="title">Acesso Rápido</div>
-            </div></div>
-        </div><span className="br-divider vertical mx-half mx-sm-1"></span>
-        <div className="header-functions dropdown">
-          <button className="br-button circle small" type="button" data-toggle="dropdown" aria-label="Abrir Funcionalidades do Sistema"><i className="fas fa-th" aria-hidden="true"></i>
-          </button>
-          <div className="br-list">
-            <div className="header">
+    <Header2></Header2>
+    <header className="br-header">
+      <div className="container-lg">
+        <div className="header-top">           
+          <div className="header-actions">
+            <div className="header-links dropdown">
+              <div className="br-list">
+                <div className="header">
+                  <div className="title">Acesso Rápido</div>
+                </div></div>
+            </div><span className="br-divider vertical mx-half mx-sm-1"></span>
+            <div className="header-functions dropdown">
+            
+              <div className="br-list">
+                <div className="header">
+                </div>
+                
+              </div>
             </div>
-            <div className="br-item">
-              <button className="br-button circle small" type="button" aria-label="Funcionalidade 4"><i className="fas fa-adjust" aria-hidden="true"></i><span className="text">Funcionalidade 4</span>
+            <div className="header-search-trigger">
+              <button className="br-button circle" type="button" aria-label="Abrir Busca" data-toggle="search" data-target=".header-search"><i className="fas fa-search" aria-hidden="true"></i>
               </button>
+            </div>
+          
+          </div>
+        </div>
+        <div className="header-bottom">
+          <div className="header-menu">
+            <div className="header-menu-trigger">
+              <button className="br-button small circle" type="button" aria-label="Menu" data-toggle="menu" data-target="#main-navigation" id="navigation"><i className="fas fa-bars" aria-hidden="true"></i>
+              </button>
+            </div>
+            <div className="header-info">
+              <div className="header-title">Nome Projeto Dnit</div>
             </div>
           </div>
         </div>
-        <div className="header-search-trigger">
-          <button className="br-button circle" type="button" aria-label="Abrir Busca" data-toggle="search" data-target=".header-search"><i className="fas fa-search" aria-hidden="true"></i>
-          </button>
-        </div>
-       
       </div>
-    </div>
-    <div className="header-bottom">
-      <div className="header-menu">
-        <div className="header-menu-trigger">
-          <button className="br-button small circle" type="button" aria-label="Menu" data-toggle="menu" data-target="#main-navigation" id="navigation"><i className="fas fa-bars" aria-hidden="true"></i>
-          </button>
-        </div>
-        <div className="header-info">
-          <div className="header-title">Nome Projeto Dnit</div>
-        </div>
-      </div>
-    </div>
-  </div>
-</header>
+    </header>
 </>
+
   );
 }
 export default Header;
