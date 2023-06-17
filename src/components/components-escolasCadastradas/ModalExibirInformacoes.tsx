302e082f1ccceae651f3181fbb7406f245228315
Carolina Barbosa <carolinabarbosab09@gmail.com>
2023-06-16 21:55:03 -0300

    #17 adicionando icones e redimencionando modal
    Co-authored-by: GuilhermeSoaress <guilhermesoares278@gmail.com>
    Co-authored-by: Antonio Neto <netocaastro10@gmail.com>

--- src/components/components-escolasCadastradas/ModalExibirInformacoes.tsx
+++ src/components/components-escolasCadastradas/ModalExibirInformacoes.tsx
@@ -20,36 +20,61 @@ const Modal = () => {
       {isModalOpen && (
         <div className="modal">
           <div className="modal-content">
-            <a href="#" className="close" onClick={closeModal}>Fechar</a>
+            
             <div>
+        <div className="container">
         <div className="div br-modal large">
             <div className="br-modal-header">CED 02 de taguatinga
             </div>
             <div className="br-modal-body">
                 <div className="br-input">
-                    <label htmlFor="input-default">Código</label>
+                    <label htmlFor="input-icon">Código</label>
+                    <div className="input-icon"><i className="fas fa-barcode" aria-hidden="true"></i>
+                    </div>
                     <input id="input-default" type="text" placeholder="Exemplo" disabled/>
                     <label htmlFor="input-default">Rede</label>
+                    <div className="input-icon"><i className="fas fa-school" aria-hidden="true"></i>
+                    </div>
                     <input id="input-default" type="text" placeholder="Exemplo" disabled/>
                     <label htmlFor="input-default">UF</label>
+                    <div className="input-icon"><i className="fas fa-map-pin" aria-hidden="true"></i>
+                    </div>
                     <input id="input-default" type="text" placeholder="Exemplo" disabled/>
                     <label htmlFor="input-default">Município</label>
+                    <div className="input-icon"><i className="fas fa-city" aria-hidden="true"></i>
+                    </div>
                     <input id="input-default" type="text" placeholder="Exemplo" disabled/> 
                     <label htmlFor="input-default">Endereço</label>
+                    <div className="input-icon"><i className="fas fa-home" aria-hidden="true"></i>
+                    </div>
                     <input id="input-default" type="text" placeholder="Exemplo" disabled/> 
                     <label htmlFor="input-default">Telefone</label>
+                    <div className="input-icon"><i className="fas fa-phone" aria-hidden="true"></i>
+                    </div>
                     <input id="input-default" type="text" placeholder="Exemplo" disabled/> 
                     <label htmlFor="input-default">CEP</label>
+                    <div className="input-icon"><i className="fas fa-thumbtack" aria-hidden="true"></i>
+                    </div>
                     <input id="input-default" type="text" placeholder="Exemplo" disabled/>
                     <label htmlFor="input-default">Localização</label>
+                    <div className="input-icon"><i className="fas fa-map" aria-hidden="true"></i>
+                    </div>
                     <input id="input-default" type="text" placeholder="Exemplo" disabled/>
                     <label htmlFor="input-default">Latidudde</label>
+                    <div className="input-icon"><i className="fas fa-map-pin" aria-hidden="true"></i>
+                    </div>
                     <input id="input-default" type="text" placeholder="Exemplo" disabled/>
                     <label htmlFor="input-default">Longitude</label>
+                    <div className="input-icon"><i className="fas fa-map-pin" aria-hidden="true"></i>
+                    </div>
                     <input id="input-default" type="text" placeholder="Exemplo" disabled/>
                     <label htmlFor="input-default">Número total de alunos</label>
+                    <div className="input-icon"><i className="fas fa-users" aria-hidden="true"></i>
+                    </div>
                     <input id="input-default" type="text" placeholder="Exemplo" disabled/>
-                    <label htmlFor="input-default">Número total de docentes</label>
+                    <label htmlFor="input-icon">Número total de docentes</label>
+                    <div className="input-icon"><i className="fas fa-users" aria-hidden="true"></i>
+                    </div>
                     <input id="input-default" type="text" placeholder="Exemplo"/>
                     <div className="br-input">
                       <label htmlFor="select-simple">Situação</label>
@@ -72,26 +97,31 @@ const Modal = () => {
                       </div>
                       <div className="br-item">
                         <div className="br-radio">
-                          <input id="rb0" type="radio" name="estados-simples" value="rb0"/>
-                          <label htmlFor="rb0">Jornada de crescimento do professor</label>
+                          <input id="rb2" type="radio" name="estados-simples" value="rb2"/>
+                          <label htmlFor="rb2">Jornada de crescimento do professor</label>
                         </div>
                       </div>
                       <div className="br-item" >
                         <div className="br-radio">
-                          <input id="rb1" type="radio" name="estados-simples" value="rb1"/>
-                          <label htmlFor="rb1">Escola crítica</label>
+                          <input id="rb3" type="radio" name="estados-simples" value="rb3"/>
+                          <label htmlFor="rb3">Escola crítica</label>
                         </div>
                       </div>
                         </div>
+                        <label htmlFor="input-icon">Observacao</label>
+                    <div className="input-icon"><i className="fas fa-info-circle" aria-hidden="true"></i>
+                    </div>
+                    <input id="input-default" type="text" placeholder="Exemplo"/>
                     
                 </div>
             </div>
             <div className="br-modal-footer justify-content-center">
-                <button className="br-button secondary" type="button">Cancelar
+                <button className="br-button secondary" type="button" onClick={closeModal}>Cancelar
                 </button>
                 <button className="br-button primary ml-2" type="button">Salvar
                 </button>
             </div>
+        </div>
         </div>
         </div>
         </div>
