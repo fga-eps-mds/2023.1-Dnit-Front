8d47b1f3c2158ac937cd046953a7a6dc7b6b266d
Miguel de Frias <miguelfrias8@gmail.com>
2023-06-25 22:23:30 -0300

    #15 Finalizando integração
    Co-authored-by: MateusPy <mateusorlando2014@gmail.com>
    Co-authored-by: Isadora Soares <isadoragalvaoss@gmail.com>

--- src/components/form/Steps/Step2.tsx
+++ src/components/form/Steps/Step2.tsx
@@ -1,22 +1,29 @@
 import { Button, Form, Input, Select, Space, notification } from "antd";
-import { useState } from "react";
-import fetchUnidadeFederativa from "../../../service/federativeUnit";
+import { useState, useEffect } from "react";
 import fetchCadastroEscola from "../../../service/registerSchool";
-
-
-
+import fetchEtapasDeEnsino from "../../../service/etapasDeEnsino";
+import fetchMunicipio from "../../../service/municipio";
+import { Municipio , EtapasDeEnsino, FederativeUnit} from "../../../models/service";
+import fetchFederativeUnit from "../../../service/federativeUnit";
+import { useFiltroTabela} from "../../../context/FiltroTabela";
+import { options } from "yargs";
 
 
 const { Option } = Select;
 interface Step2Props {
     onClickBack: () => void
 }
-interface UfProps {
-    value: number;
-    label: string;
-}
-
 export default function Step2({ onClickBack }: Step2Props) {
+    const {
+    
+        UFSelecionada,
+        setUFSelecionada,
+
+    
+        carregandoEscolas,
+    
+      } = useFiltroTabela();
+
     
     const [form] = Form.useForm();
     const [api, contextHolder] = notification.useNotification();
@@ -27,14 +34,67 @@ export default function Step2({ onClickBack }: Step2Props) {
         },
     ];
 
-    const [uf, setUf] = useState<UfProps[]>();
-    
-    async function fetchUf() {
-        const uf = await fetchUnidadeFederativa();
-        const newuf = uf.map((u) => ({ value: u.id, label: u.nome }));
-        setUf(newuf);
+
+    const [opcoesUf, setOpcoesUf] = useState<FederativeUnit[]>([]);
+    const [showOptionsUF, setShowOptionsUF] = useState(false);
+    const getUf = async () => {
+      try {
+        const resposta = await fetchFederativeUnit();
+        setOpcoesUf(resposta);
+      }
+      catch (error) {
         
+      }
     }
+    useEffect(() => {
+        if (opcoesUf.length == 0)
+          getUf();
+      })
+
+    const [opcoesMunicipio, setOpcoesMunicipio] = useState<Municipio[]>([]);
+    const [showOpcoesMunicipio, setShowOpcoesMunincipio] = useState(false);
+    const getMunicipio = async () => {
+        console.log(UFSelecionada)
+        try {
+          if (UFSelecionada) {
+            const resposta = await fetchMunicipio(UFSelecionada.id);
+            console.log(resposta);
+            setOpcoesMunicipio(resposta);
+          }
+        }
+        catch (error) {
+        }
+      }
+      useEffect(() => {
+        console.log(UFSelecionada)
+        if (opcoesMunicipio.length == 0 || carregandoEscolas)
+          getMunicipio();
+      },[UFSelecionada, carregandoEscolas])
+
+      const [OpcoesEtapasDeEnsino, setOpcoesEtapasDeEnsino] = useState<EtapasDeEnsino[]>([]);
+      const [showOpcoesEtapasDeEnsino, setShowOpcoesEtapasDeEnsino] = useState(false);
+      const getEtapasDeEnsino = async () => {
+        try {
+          const resposta = await fetchEtapasDeEnsino();
+          setOpcoesEtapasDeEnsino(resposta);
+        }
+        catch (error) {
+          
+        }   
+      }
+    
+      const alternarEstado = (valorAtual: boolean) => (!valorAtual);
+
+      const handleButtonClick = () => {
+            setShowOptionsUF(alternarEstado);
+      };
+    
+      const handleOptionClick = (option: any) => {
+        console.log(option)
+            setUFSelecionada(option);
+            setShowOptionsUF(false);
+      };
+
 
 
     const onFinish = async (values: any) => {
@@ -45,10 +105,10 @@ export default function Step2({ onClickBack }: Step2Props) {
             IdUf: values.uf,
             Cep: values.cep,
             Telefone: values.telefone,
-            IdEtapasDeEnsino: values.ciclo,
+            IdEtapasDeEnsino: values.ciclos,
             IdPorte: values.porte,
             Endereco: values.endereco,
-            //IdMunicipio: 32,
+            IdMunicipio: values.municipio,
             IdLocalizacao: values.localizacao,
             Longitude: values.longitude,
             Latitude: values.latitude,
@@ -106,25 +166,24 @@ export default function Step2({ onClickBack }: Step2Props) {
                             rules={rules}
                             label="UF"
                         >
-
                             <Select
-                                onClick={fetchUf}
+                                
                                 notFoundContent={<p>Carregando...</p>}
                                 placement="bottomRight"
                                 optionLabelProp="label"
                                 className="uf"
                             >
-                                {uf?.map((u) => (
+                                {opcoesUf?.map((u) => (
                                     <Option
-                                        key={u.value}
-                                        value={u.value}
+                                        key={u.id}
+                                        value={u.id}
                                         label={
                                             <>
-                                                {u.label}
+                                                {u.nome}
                                             </>
                                         }
                                     >
-                                        {u.label}
+                                        <a onClick={()=>handleOptionClick(u)} style={{color:'black'}}>{u.nome}</a>
                                     </Option>
                                 ))}
                             </Select>
@@ -146,16 +205,27 @@ export default function Step2({ onClickBack }: Step2Props) {
                             />
                         </Form.Item>
 
-                        <Form.Item name="ciclos" label="Ciclos de Ensino" rules={rules}>
-                            <Select
-                                mode="multiple"
+                        <Form.Item name="ciclos" label="Etapas de Ensino" rules={rules}>
+                        <Select
+                                onClick={getEtapasDeEnsino}
+                                notFoundContent={<p>Carregando...</p>}
+                                placement="bottomRight"
+                                optionLabelProp="label"
+                                className="uf"
                             >
-
-                                <Option value={1}>Ensino Infantil</Option>
-                                <Option value={2}>Ensino Fundamental - 1º, 2º e 3º ano</Option>
-                                <Option value={3}>Ensino Fundamental - 4º, 5º e 6º ano</Option>
-                                <Option value={4}>Ensino Fundamental - 7º, 8º e 9º ano</Option>
-
+                                {OpcoesEtapasDeEnsino?.map((u) => (
+                                    <Option
+                                        key={u.id}
+                                        value={u.id}
+                                        label={
+                                            <>
+                                                {u.descricao}
+                                            </>
+                                        }
+                                    >
+                                        {u.descricao}
+                                    </Option>
+                                ))}
                             </Select>
                         </Form.Item>
 
@@ -184,9 +254,26 @@ export default function Step2({ onClickBack }: Step2Props) {
                             label="Município"
                             rules={rules}
                         >
-                            <Input
-                                className="inputForm2"
-                            />
+                            <Select
+                                notFoundContent={<p>Carregando...</p>}
+                                placement="bottomRight"
+                                optionLabelProp="label"
+                                className="uf"
+                            >
+                                {opcoesMunicipio?.map((u) => (
+                                    <Option
+                                        key={u.id}
+                                        value={u.id}
+                                        label={
+                                            <>
+                                                {u.nome}
+                                            </>
+                                        }
+                                    >
+                                        {u.nome}
+                                    </Option>
+                                ))}
+                            </Select>
                         </Form.Item>
 
                         <Form.Item name="localizacao" label="Localização" rules={rules}>
--- src/pages/RegisterSchool.tsx
+++ src/pages/RegisterSchool.tsx
@@ -1,6 +1,7 @@
 import Header from "../components/Header";
 import "../styles/App.css";
 import RegS from "../components/form/RegisterEscola";
+import { FiltroProvider } from "../context/FiltroTabela";
 
 function RegisterSchool(){
     return(
@@ -8,7 +9,9 @@ function RegisterSchool(){
             <Header />
             <div className="Secao2">
                 <div className="box">
+                <FiltroProvider>
                 <RegS />
+                </FiltroProvider>
                 </div>
             </div>
         </div>
