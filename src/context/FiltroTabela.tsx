be4d006129272527a1a3fafb8967bc43d8793c38
isadoragalvaoss <isadoragalvaoss@gmail.com>
2023-06-26 03:06:29 -0300

    #17 Correção sonar
    Co-authored-by: Carolina Barbosa <carolinabarbosab09@gmail.com>
    Co-authored-by: guilh <guilhermesoares278@gmail.com>

--- src/context/FiltroTabela.tsx
+++ src/context/FiltroTabela.tsx
@@ -1,178 +1,194 @@
-import React, { createContext, useState, useContext, Dispatch, SetStateAction, useEffect } from 'react';
-import { EscolaData, FederativeUnit, Situacao, EtapasDeEnsino, Municipio } from '../models/service';
-import axios, { AxiosResponse } from 'axios';
-import { EscolasFiltradasURL } from '../consts/service';
-import { useFetcher } from 'react-router-dom';
+import axios, { AxiosResponse } from "axios";
+import {
+  Dispatch,
+  SetStateAction,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { EscolasFiltradasURL } from "../consts/service";
+import {
+  EscolaData,
+  EtapasDeEnsino,
+  FederativeUnit,
+  Municipio,
+  Situacao,
+} from "../models/service";
 
 interface FiltroContextType {
-    nomeEscola: string;
-    setNomeEscola: Dispatch<SetStateAction<string>>;
+  nomeEscola: string;
+  setNomeEscola: Dispatch<SetStateAction<string>>;
 
-    UFSelecionada: FederativeUnit | false;
-    setUFSelecionada: Dispatch<SetStateAction<FederativeUnit | false>>
+  UFSelecionada: FederativeUnit | false;
+  setUFSelecionada: Dispatch<SetStateAction<FederativeUnit | false>>;
 
-    situacaoSelecionada: Situacao | false;
-    setSituacaoSelecionada: Dispatch<SetStateAction<Situacao | false>>;
+  situacaoSelecionada: Situacao | false;
+  setSituacaoSelecionada: Dispatch<SetStateAction<Situacao | false>>;
 
-    etapaDeEnsinoSelecionada: EtapasDeEnsino | false;
-    setEtapaDeEnsinoSelecionada: Dispatch<SetStateAction<EtapasDeEnsino | false>>;
+  etapaDeEnsinoSelecionada: EtapasDeEnsino | false;
+  setEtapaDeEnsinoSelecionada: Dispatch<SetStateAction<EtapasDeEnsino | false>>;
 
-    municipioSelecionado: Municipio | false;
-    setMunicipioSelecionado: Dispatch<SetStateAction<Municipio | false>>;
+  municipioSelecionado: Municipio | false;
+  setMunicipioSelecionado: Dispatch<SetStateAction<Municipio | false>>;
 
-    escolasFiltradas: EscolaData[] | false;
-    totalEscolas: number;
-    totalPaginas: number;
-    paginaAtual: number;
-    mudarPagina: (incremento: number) => void;
-    irParaPagina: (numeroPagina:number) => void;
-    mudarQuantidadePorPaginas: (novaQuantia: number) => void;
-    escolasPorPagina: number;
-    fetchEscolasFiltradas: () => void;
-    carregandoEscolas: boolean;
-
-
+  escolasFiltradas: EscolaData[] | false;
+  totalEscolas: number;
+  totalPaginas: number;
+  paginaAtual: number;
+  mudarPagina: (incremento: number) => void;
+  irParaPagina: (numeroPagina: number) => void;
+  mudarQuantidadePorPaginas: (novaQuantia: number) => void;
+  escolasPorPagina: number;
+  fetchEscolasFiltradas: () => Promise<any>;
+  carregandoEscolas: boolean;
 }
 
 const FiltroContext = createContext<FiltroContextType | undefined>(undefined);
 
 const FiltroProvider = ({ children }: any) => {
+  const [nomeEscola, setNomeEscola] = useState("");
+  const [UFSelecionada, setUFSelecionada] = useState<FederativeUnit | false>(
+    false
+  );
+  const [situacaoSelecionada, setSituacaoSelecionada] = useState<
+    Situacao | false
+  >(false);
+  const [etapaDeEnsinoSelecionada, setEtapaDeEnsinoSelecionada] = useState<
+    EtapasDeEnsino | false
+  >(false);
+  const [municipioSelecionado, setMunicipioSelecionado] = useState<
+    Municipio | false
+  >(false);
 
-    const [nomeEscola, setNomeEscola] = useState("");
-    const [UFSelecionada, setUFSelecionada] = useState<FederativeUnit | false>(false);
-    const [situacaoSelecionada, setSituacaoSelecionada] = useState<Situacao | false>(false);
-    const [etapaDeEnsinoSelecionada, setEtapaDeEnsinoSelecionada] = useState<EtapasDeEnsino | false>(false);
-    const [municipioSelecionado, setMunicipioSelecionado] = useState<Municipio | false>(false);
-    
-    const [carregandoEscolas, setCarregandoEscolas] = useState<boolean>(false);
-    const [escolasFiltradas, setEscolasFiltradas] = useState<EscolaData[] | false>(false);
+  const [carregandoEscolas, setCarregandoEscolas] = useState<boolean>(false);
+  const [escolasFiltradas, setEscolasFiltradas] = useState<
+    EscolaData[] | false
+  >(false);
 
-    const [escolasPorPagina, setEscolasPorPagina] = useState(5);
+  const [escolasPorPagina, setEscolasPorPagina] = useState(5);
 
-    const [paginaAtual, setpaginaAtual] = useState(1);
-    const [totalEscolas, setTotalEscolas] = useState(0);
-    const [totalPaginas, setTotalPaginas] = useState(0);
+  const [paginaAtual, setpaginaAtual] = useState(1);
+  const [totalEscolas, setTotalEscolas] = useState(0);
+  const [totalPaginas, setTotalPaginas] = useState(0);
 
-    const [gatilho, setGatilho] = useState(false);
+  const [gatilho, setGatilho] = useState(false);
 
-    const ativarGatilho = () => {
-        setGatilho(valorAtual => !valorAtual)
-    }
+  const ativarGatilho = () => {
+    setGatilho((valorAtual) => !valorAtual);
+  };
 
-
-    async function fetchEscolasFiltradas(): Promise<any> {
-        try {
-            setCarregandoEscolas(true);
-            const response: AxiosResponse<responseData> = await axios.get(
-                EscolasFiltradasURL,
-                {
-                    params: {
-                        Pagina: paginaAtual,
-                        TamanhoPagina: escolasPorPagina,
-                        Nome: nomeEscola ? nomeEscola : "",
-                        IdSituacao: situacaoSelecionada ? situacaoSelecionada.id : "",
-                        IdEtapaEnsino: etapaDeEnsinoSelecionada ? etapaDeEnsinoSelecionada.id : "",
-                        IdMunicipio: municipioSelecionado ? municipioSelecionado.id : "",
-                        IdUf: UFSelecionada ? UFSelecionada.id : ""
-                    }
-                }
-            );
-         
-            setCarregandoEscolas(false)
-            setEscolasFiltradas(response.data.escolas)
-            setTotalEscolas(response.data.totalEscolas)
-            setTotalPaginas(response.data.totalPaginas)
-           
-        } catch (error) {
-            
-       
-            setCarregandoEscolas(false)
-            setEscolasFiltradas(false)
+  async function fetchEscolasFiltradas(): Promise<any> {
+    try {
+      setCarregandoEscolas(true);
+      const response: AxiosResponse<responseData> = await axios.get(
+        EscolasFiltradasURL,
+        {
+          params: {
+            Pagina: paginaAtual,
+            TamanhoPagina: escolasPorPagina,
+            Nome: nomeEscola ? nomeEscola : "",
+            IdSituacao: situacaoSelecionada ? situacaoSelecionada.id : "",
+            IdEtapaEnsino: etapaDeEnsinoSelecionada
+              ? etapaDeEnsinoSelecionada.id
+              : "",
+            IdMunicipio: municipioSelecionado ? municipioSelecionado.id : "",
+            IdUf: UFSelecionada ? UFSelecionada.id : "",
+          },
         }
+      );
+
+      setCarregandoEscolas(false);
+      setEscolasFiltradas(response.data.escolas);
+      setTotalEscolas(response.data.totalEscolas);
+      setTotalPaginas(response.data.totalPaginas);
+    } catch (error) {
+      setCarregandoEscolas(false);
+      setEscolasFiltradas(false);
     }
+  }
 
-    const mudarPagina = (incremento: number) => {
-     if(paginaAtual + incremento >= 1 && paginaAtual + incremento <= totalPaginas)
-        setpaginaAtual(valorAtual => (valorAtual + incremento))
-    }
-    const irParaPagina = (numeroPagina: number) => {
-        setpaginaAtual(numeroPagina)
-    }
-    const mudarQuantidadePorPaginas = (novaQuantia: number) => {
-        setpaginaAtual(1);
+  const mudarPagina = (incremento: number) => {
+    if (
+      paginaAtual + incremento >= 1 &&
+      paginaAtual + incremento <= totalPaginas
+    )
+      setpaginaAtual((valorAtual) => valorAtual + incremento);
+  };
+  const irParaPagina = (numeroPagina: number) => {
+    setpaginaAtual(numeroPagina);
+  };
+  const mudarQuantidadePorPaginas = (novaQuantia: number) => {
+    setpaginaAtual(1);
 
-        setEscolasPorPagina(novaQuantia)
+    setEscolasPorPagina(novaQuantia);
+  };
 
-    }
+  useEffect(() => {
+    if (paginaAtual != 1) setpaginaAtual(1);
+    else ativarGatilho();
+  }, [
+    escolasPorPagina,
+    nomeEscola,
+    situacaoSelecionada,
+    etapaDeEnsinoSelecionada,
+    municipioSelecionado,
+    UFSelecionada,
+  ]);
 
+  useEffect(() => {
+    fetchEscolasFiltradas();
+  }, [paginaAtual, gatilho]);
 
-    useEffect(() => {
-        
-        if (paginaAtual != 1)
-            setpaginaAtual(1);
-        else
-            ativarGatilho();
+  interface responseData {
+    escolas: EscolaData[];
+    escolasPorPagina: number;
+    totalEscolas: number;
+    totalPaginas: number;
+  }
 
-    }, [escolasPorPagina, nomeEscola, situacaoSelecionada, etapaDeEnsinoSelecionada, municipioSelecionado, UFSelecionada])
+  const contextValue: FiltroContextType = {
+    nomeEscola,
+    setNomeEscola,
 
+    UFSelecionada,
+    setUFSelecionada,
 
+    situacaoSelecionada,
+    setSituacaoSelecionada,
 
-    useEffect(() => {
-        fetchEscolasFiltradas();
+    etapaDeEnsinoSelecionada,
+    setEtapaDeEnsinoSelecionada,
 
-    }, [paginaAtual, gatilho])
+    municipioSelecionado,
+    setMunicipioSelecionado,
 
-    interface responseData {
-        escolas: EscolaData[],
-        escolasPorPagina: number,
-        totalEscolas: number,
-        totalPaginas: number
-    }
-
-
-
-    const contextValue: FiltroContextType = {
-
-        nomeEscola,
-        setNomeEscola,
-
-        UFSelecionada,
-        setUFSelecionada,
-
-        situacaoSelecionada,
-        setSituacaoSelecionada,
-
-        etapaDeEnsinoSelecionada,
-        setEtapaDeEnsinoSelecionada,
-
-        municipioSelecionado,
-        setMunicipioSelecionado,
-
-        escolasFiltradas,
-        totalEscolas,
-        totalPaginas,
-        paginaAtual,
-        mudarPagina,
-        irParaPagina,
-        mudarQuantidadePorPaginas,
-        escolasPorPagina,
-        fetchEscolasFiltradas,
-        carregandoEscolas,
-
-    };
-    return (
-        <FiltroContext.Provider value={contextValue} >{children}</ FiltroContext.Provider>
-    );
+    escolasFiltradas,
+    totalEscolas,
+    totalPaginas,
+    paginaAtual,
+    mudarPagina,
+    irParaPagina,
+    mudarQuantidadePorPaginas,
+    escolasPorPagina,
+    fetchEscolasFiltradas,
+    carregandoEscolas,
+  };
+  return (
+    <FiltroContext.Provider value={contextValue}>
+      {children}
+    </FiltroContext.Provider>
+  );
 };
 
 const useFiltroTabela = (): FiltroContextType => {
-    const context = useContext(FiltroContext);
+  const context = useContext(FiltroContext);
 
-    if (!context) {
-        throw new Error('useFiltro must be used within a FiltroProvider');
-    }
+  if (!context) {
+    throw new Error("useFiltro must be used within a FiltroProvider");
+  }
 
-    return context;
+  return context;
 };
 
-export { useFiltroTabela, FiltroProvider };
+export { FiltroProvider, useFiltroTabela };
