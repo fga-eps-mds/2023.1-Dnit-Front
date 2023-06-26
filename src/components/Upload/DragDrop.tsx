b3e4ef49c45eb502d8c129eeaa80652bf15d5f33
avmricardo <ricardomaciel.unb@gmail.com>
2023-06-26 01:02:28 -0300

    #12 Enviando arquivos para o back
    Co-authored-by: Artur <artur190503@outlook.com>
    Co-authored-by: João Gabriel de Matos <joaogabriel.c.matos@gmail.com>

--- src/components/Upload/DragDrop.tsx
+++ src/components/Upload/DragDrop.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { FileOutlined } from '@ant-design/icons';
-import type { UploadProps } from 'antd';
-import { message, Upload } from 'antd';
+import type { UploadProps, UploadFile } from 'antd';
+import { message, Upload, Button } from 'antd';
+import '../../styles/form/step3.css';
+import { UploadChangeParam } from 'antd/lib/upload';
+import axios from 'axios';
 import "../../styles/form/step3.css"
 
 const { Dragger } = Upload;
 
+interface DragDropProps {
+  onClickBack: () => void
+}
+
 const props: UploadProps = {
-  name: 'file',
+  name: 'arquivo',
   multiple: true,
-  action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+  action: 'https://localhost:7083/api/escolas/cadastrarEscolaPlanilha',
+  beforeUpload: () => false,
   onChange(info) {
-    const { status } = info.file;
+    const { status, name } = info.file;
     if (status !== 'uploading') {
       console.log(info.file, info.fileList);
     }
     if (status === 'done') {
-      message.success(`${info.file.name} file uploaded successfully.`);
+      console.log(info.file.response)
+      message.success(`${name} arquivo adicionado com sucesso.`);
     } else if (status === 'error') {
-      message.error(`${info.file.name} file upload failed.`);
+      message.error(`${name} falha ao receber arquivo.`);
     }
   },
   onDrop(e) {
@@ -26,13 +35,55 @@ const props: UploadProps = {
   },
 };
 
-const App: React.FC = () => (
-  <Dragger {...props}>
-    <p className="ant-upload-drag-icon">
-      <FileOutlined />
-    </p>
-    <p className="ant-upload-text">Você pode clicar ou arrastar arquivos aqui para adicioná-los.</p>
-  </Dragger>
-);
+const App: React.FC<DragDropProps> = ({onClickBack}: DragDropProps) => {
+  const uploadRef = useRef<any>(null);
+  const [fileList, setFileList] = useState<UploadFile<any>[]>([]);
+
+  const handleButtonClick = async () => {
+    if (fileList.length > 0) {
+      const formData = new FormData();
+      formData.append('arquivo', fileList[0].originFileObj as File);
+
+      try {
+        const response = await axios.post(
+          'https://localhost:7083/api/escolas/cadastrarEscolaPlanilha',
+          formData
+        );
+        console.log(response.data);
+        message.success('Arquivo enviado com sucesso.');
+      } catch (error) {
+        console.error(error);
+        message.error('Ocorreu um erro ao enviar o arquivo.');
+      }
+    } else {
+      message.warning('Nenhum arquivo carregado.');
+    }
+  };
+
+  const handleFileChange = ({ fileList }: UploadChangeParam) => {
+    setFileList(fileList);
+  };
+
+  return (
+    <>
+      <Dragger ref={uploadRef} {...props} fileList={fileList} onChange={handleFileChange}>
+        <p className="ant-upload-drag-icon">
+          <FileOutlined />
+        </p>
+        <p className="ant-upload-text">
+          Você pode clicar ou arrastar arquivos aqui para adicioná-los.
+        </p>
+      </Dragger>
+      <div className="container-botoes">
+        <Button className="botaoCancelar" onClick={onClickBack}>
+          Cancelar
+        </Button>
+        <Button className="botaoEnviar" type="primary" onClick={handleButtonClick}>
+          Enviar
+        </Button>
+      </div>
+    </>
+  );
+};
 
 export default App;
--- src/components/form/Steps/Step3.tsx
+++ src/components/form/Steps/Step3.tsx
@@ -7,15 +7,6 @@ interface Step1Props {
     onClickAceito: () => void
 }
 
-const enviarArquivo =async (values: any) => {
-    try{
-        //await fetchInsertFile()
-    }
-    catch{
-
-    }
-};
-
 export default function Step3({ onClickBack, onClickAceito }: Step1Props) {
     function onFinish() {
         console.log("ok");
@@ -24,15 +15,7 @@ export default function Step3({ onClickBack, onClickAceito }: Step1Props) {
         <div className="form3">
             <h2>Baixar Modelo de Arquivo</h2>
             <div className="secaoInserir">
-                <Dragdrop />
-            </div>
-            <div className="container-botoes">
-                <Button className="botaoCancelar" onClick={onClickBack}>
-                    Cancelar
-                </Button>
-                <Button className="button1" onClick={onClickAceito}>
-                    Inserir arquivo
-                </Button>
+                <Dragdrop onClickBack={onClickBack} />
             </div>
         </div>
     )
