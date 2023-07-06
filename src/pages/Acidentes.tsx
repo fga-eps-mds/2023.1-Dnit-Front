203b686292e8a23b6dfe61aa4856e1c512b9dd04
Yan Luca <yanlucaviana@gmail.com>
2023-07-06 17:26:01 -0300

    #55 Criando tela de aceitação
    Co-authored-by: migueldefrias <miguelfrias8@gmail.com>

--- src/pages/Acidentes.tsx
+++ src/pages/Acidentes.tsx
@@ -4,11 +4,11 @@ import CadAcidentes from "../components/form/CadAcidentes";
 import { useState } from "react";
 import { AcidentesProvider } from "../context/acidentesCadastrados";
 import Acidentes_erro1 from "../components/form/Steps/AcidentesErro1";
-
+import AcidentesAceito from "../components/form/Steps/AcidentesAceito";
 
 function CadastrarAcidentes(){
     const [screen, setScreen] = useState<
-    "sc1" | "sc2" | "sc3" | "sc4"
+    "sc1" | "sc2" | "sc3"
     >("sc1");
 
     return(
@@ -17,18 +17,23 @@ function CadastrarAcidentes(){
             <Header />
             <div className="Secao2">
                 <div className="box">
+                    <div className="formrs">
                 <AcidentesProvider>
                 {screen === "sc1" &&(
                 <CadAcidentes
                 onClickBack={() => setScreen("sc1")}
                 onClickError={() => setScreen("sc2")}
                 onClickAceito={() => setScreen("sc3")}
-                onClickErroJaCadastrada={() => setScreen("sc4")}
+                onClickErroJaCadastrada={() => setScreen("sc3")}
                 />)}
                 </AcidentesProvider>
                 {screen === "sc2" && (
                     <Acidentes_erro1 onClickVoltar={() => setScreen("sc1")} />
                     )}
+                {screen === "sc3" &&(
+                    <AcidentesAceito onClickVoltar={() => setScreen("sc1")}/>
+                )}
+                </div>
             </div>
             </div>
         </div>
