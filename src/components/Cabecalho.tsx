6d6c5c9c1657ddd6d079dff68af34887b7ef9b37
VitorLamego <vitorlamegoo@gmail.com>
2023-10-15 15:07:22 -0300

    Feat: create title and subtitle props

--- src/components/Cabecalho.tsx
+++ src/components/Cabecalho.tsx
@@ -1,31 +1,27 @@
-import { Button } from "antd";
-import { useNavigate } from "react-router-dom";
 import IconGov from "../assets/govIcone.png";
 import BotaoAltoContraste from "../assets/icones/BotaoAltoContraste2.svg";
 import BotaoLinks from "../assets/icones/BotaoLinks.svg"
 import BotaoMenu from "../assets/icones/BotaoMenu.svg"
+import LogoDNITAzul from "../assets/logoDnitAzul.png"
 import "./estilo/Cabecalho.css";
 import IconUsuario from "../assets/icones/usuario.svg";
-import React from "react";
 
 interface HeaderProps {
   login?: boolean;
   dashboard?: boolean;
+  title?: string;
+  subtitle?: string;
 }
 
-const Header = ({ login, dashboard }: HeaderProps) => {
-    const navigate = useNavigate();
+const Header = ({ login, dashboard, title, subtitle }: HeaderProps) => {
     return (
         <header className="header">
-
             <div className = "top">
                 <div className="grupo-esquerda">
                     <img className="gov-logo" src={IconGov}  alt='Logo gov.br'/>
                     <div className="rectangle"></div>
-                    <p className="title-header">Departamento Nacional de Infraestrutura de Transportes</p>
-                    {/* Não sei se esse anchor é necessário*/}
+                    <img className="gov-logo" src={LogoDNITAzul}  alt='Logo gov.br'/>
                 </div>
-
                 <div className="grupo-direita">
                     <div className="botao-links">
                         <img src={BotaoLinks} alt='Botão de links'/>
@@ -40,22 +36,18 @@ const Header = ({ login, dashboard }: HeaderProps) => {
                         <img className="user-icon" src={IconUsuario} alt='Ícone representando usuário'/>
                     </div>
                 </div>
-
             </div>
-
-            <div className="bottom">
+            {title && <div className="bottom">
                 <div className="bottom-info">
-                    <div className="menu">
-                        <img src={BotaoMenu} alt='Botão de Menu'/>
+                    <div className="menu-row">
+                        <img src={BotaoMenu} className="menu" alt='Botão de Menu'/>
+                        <div className="header-title">{title}</div>
                     </div>
-
-                    {/* Não consigo colocar esses como h1 e h2 direito*/}
                     <div className="bottom-text">
-                        <div className="header-title">Título Header</div>
-                        <div className="header-subtitle">Subtitulo Header</div>
+                        {subtitle && <div className="header-subtitle">{subtitle}</div>}
                     </div>
                 </div>
-            </div>
+            </div>}
         </header>
     );
 }
