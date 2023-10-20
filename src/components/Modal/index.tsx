93e615b46605c6fd22761b0d25514aa04963f121
VitorLamego <vitorlamegoo@gmail.com>
2023-10-19 23:19:50 -0300

    refactor: add props to modal component

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,26 +1,37 @@
-import React, { useState } from 'react'
+import  { ReactNode } from 'react'
 import './styles.css'
 
-export default function Modal() {
-  const [modal, setModal] = useState(false);
+interface ModalProps {
+  title: string;
+  isOpen: boolean;
+  children: ReactNode;
+  button1Text: string;
+  button2Text: string;
+  confirmAction: () => void;
+  closeModal: () => void;
+}
 
-  const toggleModal = () => { setModal(!modal) };
+export default function Modal({title, isOpen, children, button1Text, button2Text, confirmAction, closeModal } : ModalProps) {
+  if (!isOpen) return null;
 
   return (
-    <div className="overlay" onClick={toggleModal}>
-      <div className="div br-modal medium">
-        <div className="br-modal-header">Título da modal
-          <button className="br-button close circle" type="button" data-dismiss="br-modal" aria-label="Close" onClick={toggleModal}><i className="fas fa-times" aria-hidden="true"></i>
+    <div className="overlay" >
+      <div className="br-modal medium">
+        <div className="br-modal-header">
+          <span>{title}</span>
+          <button className="br-button close circle" type="button" data-dismiss="br-modal" aria-label="Close" onClick={closeModal}>
+            <i className="fas fa-times" aria-hidden="true"></i>
           </button>
         </div>
         <div className="br-modal-body">
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus commodi laboriosam vel sequi quam deleniti, laborum mollitia blanditiis dolores officiis nulla quos dolorem repellat in nisi alias nesciunt fugit. Similique!</p>
+          {children}
         </div>
         <div className="br-modal-footer justify-content-end">
-          <button className="br-button secondary" type="button">Ação 1
+          <button className="br-button secondary" type="button" onClick={closeModal}>
+            <span>{button1Text}</span>
           </button>
-          <button className="br-button primary ml-2" type="button" onClick={toggleModal}>
-            Ação2
+          <button className="br-button primary ml-2" type="button" onClick={confirmAction}>
+            <span>{button2Text}</span>
           </button>
         </div>
       </div>
