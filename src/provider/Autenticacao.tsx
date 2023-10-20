b61ce8f9ba0b352e1caa7519fb2cd55f452dac9f
Thiago Paiva <thiagohdaqw@gmail.com>
2023-10-20 20:14:11 -0300

    feat: autentica cliente do axios após o login

--- src/provider/Autenticacao.tsx
+++ src/provider/Autenticacao.tsx
@@ -1,10 +1,31 @@
 import React, { createContext } from "react";
 import { useNavigate } from "react-router-dom";
+import { LoginResponse } from "../models/auth";
+import axios from "axios";
+
+export enum AuthLocalStorage {
+  Token = "Token",
+  TokenAtualizacao = "TokenAtualizacao",
+  ExpiraEm = "ExpiraEm",
+  Permissoes = "Permissoes"
+}
 
 interface AuthContextType {
-  login: () => void;
+  login: (dados: LoginResponse) => void;
   logout: () => void;
   getAuth: () => boolean;
+}
+
+function setApiToken(token?: string | null) {
+  axios.defaults.headers.common.Authorization = token ? `Bearer ${token}` : null;
+}
+
+function salvarLogin(dados: LoginResponse) {
+  localStorage.setItem(AuthLocalStorage.Token, dados.token);
+  localStorage.setItem(AuthLocalStorage.TokenAtualizacao, dados.tokenAtualizacao);
+  localStorage.setItem(AuthLocalStorage.ExpiraEm, dados.expiraEm);
+  localStorage.setItem(AuthLocalStorage.Permissoes, dados.permissoes?.join(','));
+  setApiToken(dados.token);
 }
 
 export const AuthContext = createContext<AuthContextType>({
@@ -21,19 +42,21 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
   const navigate = useNavigate();
 
   const getAuth = () => {
-    let storage = localStorage.getItem("login");
-    return !!storage;
+    let token = localStorage.getItem(AuthLocalStorage.Token);
+    if (!!token && !axios.defaults.headers.common.Authorization) {
+      setApiToken(token);
+    }
+    return !!token;
   };
 
-  const login = () => {
-    localStorage.setItem("login", "authenticated");
-    getAuth();
+  const login = (dados: LoginResponse) => {
+    salvarLogin(dados);
     navigate("/home");
   };
 
   const logout = () => {
-    localStorage.removeItem("login");
-    getAuth();
+    setApiToken(null);
+    Object.values(AuthLocalStorage).forEach(a => localStorage.removeItem(a));
   };
 
   return (
