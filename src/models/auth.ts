a1c182d511391fd639719a3b8b3cc79e8c73206b
Thiago Paiva <thiagohdaqw@gmail.com>
2023-10-20 20:14:11 -0300

    feat: autentica cliente do axios após o login

---
+++ src/models/auth.ts
@@ -0,0 +1,18 @@
+export enum Permissao {
+  EscolaCadastrar = "EscolaCadastrar",
+  EscolaEditar = "EscolaEditar",
+  EscolaRemover = "EscolaRemover",
+  EscolaVisualizar = "EscolaVisualizar",
+  EmpresaCadastrar = "EmpresaCadastrar",
+  EmpresaEditar = "EmpresaEditar",
+  EmpresaRemover = "EmpresaRemover",
+  PerfilCadastrar = "PerfilCadastrar",
+  PerfilEditar = "PerfilEditar",
+}
+
+export interface LoginResponse {
+  token: string;
+  tokenAtualizacao: string;
+  expiraEm: string;
+  permissoes: Permissao[];
+}
