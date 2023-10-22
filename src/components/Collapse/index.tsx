50ae96435b02767c519a4b45422cacac1ff19d89
joaoseisei <109870486+joaoseisei@users.noreply.github.com>
2023-10-22 14:10:49 -0300

    feat: adicionando estilo nas opçoes do colapse

--- src/components/Collapse/index.tsx
+++ src/components/Collapse/index.tsx
@@ -1,22 +1,42 @@
-import React, {ReactNode, useState} from 'react';
-import "./style.css"
-import {H} from "msw/lib/glossary-de6278a9";
+import React, { useState } from 'react';
+import './style.css';
 
 interface CollapseInterface {
-    titulo: string
+    titulo: string;
     opcoes: string[];
 }
 
-const CollapseCustom = (props : CollapseInterface) => {
+const CollapseCustom = (props: CollapseInterface) => {
+    const { titulo, opcoes } = props;
+
     const [isCollapsed, setIsCollapsed] = useState(true);
-    const toggleCollapse = () => setIsCollapsed(!isCollapsed);
+    const toggleCollapse = () => { setIsCollapsed(!isCollapsed) };
     
-    const {titulo, opcoes} = props;
-    
+    const [checkboxStates, setCheckboxStates] = useState(opcoes.map(() => false));
+    const handleCheckboxChange = (index: number) => {
+        const newCheckboxStates = [...checkboxStates];
+        newCheckboxStates[index] = !newCheckboxStates[index];
+        
+        const celula = document.getElementById(`conteudo-${index}`);
+        const conteudo = document.getElementById(`filho-${index}`);
+        const label = document.getElementById(`label-${index}`);
+        
+        if (celula && conteudo && label) {
+            const novaCorFundo = !newCheckboxStates[index] ? "#F8F8F8" : "#2670E8";
+            const novaCorTexto = !newCheckboxStates[index] ? "#000000" : "#FFFFFF";
+            
+            celula.style.backgroundColor = novaCorFundo;
+            conteudo.style.backgroundColor = novaCorFundo;
+            label.style.color = novaCorTexto;
+        }
+        
+        setCheckboxStates(newCheckboxStates);
+    };
+
     const elementosFilhos = opcoes.map((item, index) => (
-        <div className={`FILHO${index}`} id={"filhos"}>
-            <div className="conteudo">
-                <div key={index} className="align-items-center br-item" role="listitem" id={`filho${index}`}>
+        <div className="CollapseOpcoes" key={index}>
+            <div className="conteudo" id={`conteudo-${index}`}>
+                <div className="align-items-center br-item" role="listitem" id={`filho-${index}`}>
                     <div className="row align-items-center">
                         <div className="mb-1">
                             <div className="br-checkbox">
@@ -24,46 +44,55 @@ const CollapseCustom = (props : CollapseInterface) => {
                                     id={`checkbox-${index}`}
                                     name={`checkbox-${index}`}
                                     type="checkbox"
-                                    data-child="check-01"
+                                    data-child={titulo}
+                                    checked={checkboxStates[index]}
+                                    onChange={() => {
+                                        handleCheckboxChange(index);
+                                    }}
                                 />
-                                <label className={`nome-${index}`} htmlFor={`checkbox-${index}`} style={{ fontFamily: 'Rawline, sans-serif'}}>{item}</label>
+                                <label id={`label-${index}`} htmlFor={`checkbox-${index}`} style={{fontFamily: 'Rawline'}}>{item}</label>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            
             <span className="br-divider"></span>
         </div>
     ));
-    
+
     return (
         <div className="collapse-example">
+            
             <div className="align-items-center br-item" role="listitem" onClick={toggleCollapse}>
                 <div className="content">
-                    <div className="flex-fill">     {/*TITULO*/}
-                        
+                    <div className="flex-fill" id={"CollapsePai"}>
                         <div className="br-checkbox">
-                            <input id="checkbox-ind1" name="checkbox-ind1" type="checkbox" aria-label="Selecionar tudo" data-parent="check-01"/>
-                            <label htmlFor="checkbox-ind1">{titulo}</label>
+                            <input id="checkbox-ind1" 
+                                   name="checkbox-ind1" 
+                                   type="checkbox" 
+                                   data-parent={titulo}
+                                   onChange={()=>{
+                                       
+                                   }}
+                            />
+                            <label htmlFor="checkbox-ind1" style={{ fontFamily: 'Rawline, sans-serif'}}>
+                                {titulo}
+                            </label>
                         </div>
-                        
                     </div>
-                    
                     <i className={`fas ${isCollapsed ? 'fa-angle-down' : 'fa-angle-up'}`} aria-hidden="true"></i>
                 </div>
             </div>
-            <span className="br-divider"></span>
             
+            <span className="br-divider"></span>
+
             {isCollapsed && (
                 <div className="br-list" role="list" data-sub="data-sub">
                     {elementosFilhos}
                 </div>
             )}
-            
         </div>
     );
-  
-}
+};
 
 export default CollapseCustom;
