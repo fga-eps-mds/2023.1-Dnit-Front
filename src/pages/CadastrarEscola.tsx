77a415f0d8f40d3876f7f8f4d1af396645b3d537
Thiago Paiva <thiagohdaqw@gmail.com>
2023-10-21 22:16:45 -0300

    feat: verifica permissoes nas paginas

--- src/pages/CadastrarEscola.tsx
+++ src/pages/CadastrarEscola.tsx
@@ -1,9 +1,22 @@
+import React, { useContext, useEffect } from "react";
 import Header from "../components/Cabecalho";
 import RegS from "../components/cadastrarEscolas/CadastrarEscola";
 import { FiltroProvider } from "../context/FiltroTabela";
 import "../styles/App.css";
+import { AuthContext } from "../provider/Autenticacao";
+import { useNavigate } from "react-router-dom";
+import { Permissao } from "../models/auth";
 
 function RegisterSchool() {
+  const navigate = useNavigate();
+  const { temPermissao } = useContext(AuthContext);
+
+  useEffect(() => {
+    if (!temPermissao(Permissao.EscolaCadastrar)) {
+      navigate("/");
+    }
+  }, []);
+
   return (
     <div className="App">
       <Header dashboard />
--- src/pages/TelaUPS.tsx
+++ src/pages/TelaUPS.tsx
@@ -1,8 +1,21 @@
+import { useContext, useEffect } from "react";
 import Header from "../components/Cabecalho";
 import UPSForm from "../components/visualizarUps/FormularioUPS";
+import { Permissao } from "../models/auth";
 import "../styles/App.css";
+import { AuthContext } from "../provider/Autenticacao";
+import { useNavigate } from "react-router-dom";
 
 function TelaUPS() {
+  const navigate = useNavigate();
+  const { temPermissao } = useContext(AuthContext);
+
+  useEffect(() => {
+    if (!temPermissao(Permissao.UpsVisualizar)) {
+      navigate("/");
+    }
+  }, []);
+
   return (
     <div className="App">
       <Header dashboard />
