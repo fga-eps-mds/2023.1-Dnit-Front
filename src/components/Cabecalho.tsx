a523bfca3599b3aba08e2ca99784553f873306af
VitorLamego <vitorlamegoo@gmail.com>
2023-10-23 20:43:26 -0300

    feat: add user name first letter on header

--- src/components/Cabecalho.tsx
+++ src/components/Cabecalho.tsx
@@ -1,11 +1,12 @@
 import IconGov from "../assets/govIcone.png";
 import BotaoAltoContraste from "../assets/icones/BotaoAltoContraste2.svg";
-import BotaoLinks from "../assets/icones/BotaoLinks.svg"
-import BotaoMenu from "../assets/icones/BotaoMenu.svg"
-import LogoDNITAzul from "../assets/logoDnitAzul.png"
+import BotaoLinks from "../assets/icones/BotaoLinks.svg";
+import BotaoMenu from "../assets/icones/BotaoMenu.svg";
+import LogoDNITAzul from "../assets/logoDnitAzul.png";
 import "./estilo/Cabecalho.css";
 import IconUsuario from "../assets/icones/usuario.svg";
 import { useNavigate } from "react-router-dom";
+import { AuthLocalStorage } from "../provider/Autenticacao";
 
 interface HeaderProps {
   hasLogged?: boolean;
@@ -13,48 +14,62 @@ interface HeaderProps {
   subtitle?: string;
 }
 
-const Header = ({ hasLogged = true, title, subtitle }: HeaderProps) => {
-    const navigate = useNavigate();
-    return (
-        <header className="header">
-            <div className = "top">
-                <div className="grupo-esquerda" data-testid="redirecionar"
-        onClick={() => (hasLogged ? navigate("/dashboard") : navigate("/"))}>
-                    <img className="gov-logo" src={IconGov}  alt='Logo gov.br'/>
-                    <div className="rectangle"></div>
-                    <img className="gov-logo" src={LogoDNITAzul}  alt='Logo gov.br'/>
-                </div>
-                <div className="grupo-direita">
-                    <div className="botao-links">
-                        <img src={BotaoLinks} alt='Botão de links'/>
-                    </div>
+const userName = localStorage.getItem(AuthLocalStorage.Nome);
 
-                    <div className="alto-contraste-area">
-                        <img src={BotaoAltoContraste} alt='Botão de alto contraste'/>
-                    </div>
-                
-                   {hasLogged ? 
-                   <div className="profile">
-                    <span className="letter">L</span>
-                   </div> : <div className="botao-login" onClick={() => navigate("/login")}>
-                        <div className="login-text">Entrar</div>
-                        <img className="user-icon" src={IconUsuario} alt='Ícone representando usuário'/>
-                    </div>}
-                </div>
+const Header = ({ hasLogged = true, title, subtitle }: HeaderProps) => {
+  const navigate = useNavigate();
+  return (
+    <header className="header">
+      <div className="top">
+        <div
+          className="grupo-esquerda"
+          data-testid="redirecionar"
+          onClick={() => (hasLogged ? navigate("/dashboard") : navigate("/"))}
+        >
+          <img className="gov-logo" src={IconGov} alt="Logo gov.br" />
+          <div className="rectangle"></div>
+          <img className="gov-logo" src={LogoDNITAzul} alt="Logo gov.br" />
+        </div>
+        <div className="grupo-direita">
+          <div className="botao-links">
+            <img src={BotaoLinks} alt="Botão de links" />
+          </div>
+
+          <div className="alto-contraste-area">
+            <img src={BotaoAltoContraste} alt="Botão de alto contraste" />
+          </div>
+
+          {hasLogged ? (
+            <div className="profile">
+              <span className="letter">{userName?.charAt(0) ?? "U"}</span>
             </div>
-            {title && <div className="bottom">
-                <div className="bottom-info">
-                    <div className="menu-row">
-                        <img src={BotaoMenu} className="menu" alt='Botão de Menu'/>
-                        <div className="header-title">{title}</div>
-                    </div>
-                    <div className="bottom-text">
-                        {subtitle && <div className="header-subtitle">{subtitle}</div>}
-                    </div>
-                </div>
-            </div>}
-        </header>
-    );
-}
+          ) : (
+            <div className="botao-login" onClick={() => navigate("/login")}>
+              <div className="login-text">Entrar</div>
+              <img
+                className="user-icon"
+                src={IconUsuario}
+                alt="Ícone representando usuário"
+              />
+            </div>
+          )}
+        </div>
+      </div>
+      {title && (
+        <div className="bottom">
+          <div className="bottom-info">
+            <div className="menu-row">
+              <img src={BotaoMenu} className="menu" alt="Botão de Menu" />
+              <div className="header-title">{title}</div>
+            </div>
+            <div className="bottom-text">
+              {subtitle && <div className="header-subtitle">{subtitle}</div>}
+            </div>
+          </div>
+        </div>
+      )}
+    </header>
+  );
+};
 
 export default Header;
