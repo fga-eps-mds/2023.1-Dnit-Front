a9af201c3aa0ca34d0ee7b9ba18f9caed6fa36ab
Thiago Paiva <54081877+thiagohdaqw@users.noreply.github.com>
2023-10-24 22:52:24 -0300

    refactor: desacopla modal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,38 +1,17 @@
-import  { ReactNode } from 'react'
+import { ReactNode } from 'react';
 import './styles.css'
 
 interface ModalProps {
-  title: string;
-  isOpen: boolean;
+  className: string;
   children: ReactNode;
-  button1Text: string;
-  button2Text: string;
-  confirmAction: () => void;
-  closeModal: () => void;
 }
 
-export default function Modal({title, isOpen, children, button1Text, button2Text, confirmAction, closeModal } : ModalProps) {
-  if (!isOpen) return null;
-
+export default function Modal({ className = 'default', children }: ModalProps) {
   return (
-    <div className="overlay" >
+    <div className={"overlay " + className} >
       <div className="br-modal medium">
-        <div className="br-modal-header">
-          <span>{title}</span>
-          <button className="br-button close circle" type="button" data-dismiss="br-modal" aria-label="Close" onClick={closeModal}>
-            <i className="fas fa-times" aria-hidden="true"></i>
-          </button>
-        </div>
-        <div className="br-modal-body">
+        <div className="br-modal-body d-flex flex-column w-100">
           {children}
-        </div>
-        <div className="br-modal-footer justify-content-end">
-          <button className="br-button secondary" type="button" onClick={closeModal}>
-            <span>{button1Text}</span>
-          </button>
-          <button className="br-button primary ml-2" type="button" onClick={confirmAction}>
-            <span>{button2Text}</span>
-          </button>
         </div>
       </div>
     </div>
