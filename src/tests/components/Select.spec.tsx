f60f373a8cdc1dca699b6db52c9617c611b26bfa
RafaelBP02 <rafaelbertopereira@gmail.com>
2023-11-06 23:50:00 -0300

    test: Cria no teste para o componente Select
    fechar o dropdown quando um clique ocorre fora do componente

--- src/tests/components/Select.spec.tsx
+++ src/tests/components/Select.spec.tsx
@@ -1,13 +1,18 @@
 /* eslint-disable testing-library/no-unnecessary-act */
 /* eslint-disable testing-library/render-result-naming-convention */
-import { render, fireEvent } from "@testing-library/react";
+import { render, fireEvent, queryByText } from "@testing-library/react";
 import Select from "../../components/Select";
+import { Dropdown } from "antd";
+
+const estadoInicial = '';
+const itemsTest = [{ id: "0", rotulo: "test0" }, { id: "1", rotulo: "test1" }, { id: "2", rotulo: "test2" }];
+const onChangeMock = jest.fn();
+
+afterEach(jest.clearAllMocks);
 
 describe("Testes para o componente Select", () => {
   it("Deve renderizar o Select Corretamente", () => {
-    const estadoInicial = '';
-    const itemsTest = [{ id: "0", rotulo: "test0" }, { id: "1", rotulo: "test1" }, { id: "2", rotulo: "test2" }]
-
+    
 
     const screen = render(
       <Select 
@@ -24,16 +29,12 @@ describe("Testes para o componente Select", () => {
   });
 
   it("Deve selecionar opção do dropdown do Select", () => {
-    const estadoInicial = '';
-    const itemsTest = [{ id: "0", rotulo: "test0" }, { id: "1", rotulo: "test1" }, { id: "2", rotulo: "test2" }]
-
-
     const screen = render(
       <Select 
         items={itemsTest}
         value={estadoInicial}
         label={"Título do Select"}
-        onChange={() => {}}
+        onChange={onChangeMock}
       />
     )
 
@@ -44,8 +45,28 @@ describe("Testes para o componente Select", () => {
     expect(screen.getByText("test1")).toBeInTheDocument();
     expect(screen.getByText("test2")).toBeInTheDocument();
 
-    const opcao = screen.getByText("test0");
+    const opcao = screen.getByText("test1");
     fireEvent.click(opcao);
 
+    expect(onChangeMock).toHaveBeenCalledWith("1");
   });
+
+  it('Deve fechar o dropdown quando um clique ocorre fora do componente', () => {
+    const screen = render(
+      <div>
+        <div data-testid="outside">Área externa</div>
+        <Select items={itemsTest}
+        value={itemsTest[0].id}
+        onChange={onChangeMock} />
+      </div>
+    );
+
+    const dropDown = screen.getByTestId("customSelect");
+    fireEvent.click(dropDown);
+
+    fireEvent.mouseDown(screen.getByTestId('outside'));
+    expect(screen.getByDisplayValue(itemsTest[0].rotulo)).toBeInTheDocument();
+  });
+
+
 });
