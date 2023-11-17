0f65426834f76785464f0a7e67ab0f34e74656f3
joaoseisei <109870486+joaoseisei@users.noreply.github.com>
2023-11-17 20:41:08 -0300

    fix: trocando a interface e corrigindo promisse de escolaData
    Co-authored-by: Eduardo Sandes <eduardo.sandes6@gmail.com>
    Co-authored-by: Artur Bartz <arturbylly2@gmail.com>

--- src/components/EscolaRanqueModal/index.tsx
+++ src/components/EscolaRanqueModal/index.tsx
@@ -2,73 +2,47 @@ import React, { useState, useEffect } from 'react';
 import { EscolaData } from '../../pages/Ranque/index'; 
 import "../../styles/App.css";
 import "../../pages/Ranque/index.css";
-import { EscolaRanqueData } from '../../models/ranque';
 import Modal from "../../components/Modal/index";
+import {fetchData} from "../../service/escolaApi";
 
 interface ModalProps {
-    isOpen: boolean;
     onClose: () => void;
     onCreateAcao: () => void;
-    escola : EscolaRanqueData | undefined;
+    escolaId : string;
 }
 
-const ModalRanqueEscola: React.FC<ModalProps> = ({ isOpen, onClose, onCreateAcao, escola }) => {
+const ModalRanqueEscola: React.FC<ModalProps> = ({ escolaId, onClose, onCreateAcao,}) => {
 
-  // const [escolaSelecionada, setEscolaSelecionada] = useState<EscolaData | undefined>();
-  //
-  // const fetchEscolaSelecionada = async () => {
-  //   const escolas = await fetchData(escola?.escola?.id);
-  //   setEscolaSelecionada(escolas);
-  // }
-  // useEffect(()=>{
-  //   fetchEscolaSelecionada()
-  // }, []);
-  //
-  // setEscolaSelecionada(escolaDataExemplo);
+  const [escolaSelecionada, setEscolaSelecionada] = useState<EscolaData | undefined>();
 
-  const escolaSelecionada: EscolaData = {
-    id: '123',
-    posicao: '1',
-    pontuacao: 90,
-    escola: 'Escola Exemplo',
-    etapasEnsino: 'Fundamental',
-    municipio: 'Cidade Exemplo',
-    uf: 'UF',
-    código: 'ABC123',
-    alunos: 500,
-    porte: 'Médio',
-    situação: 'Ativa',
-    endereço: 'Rua Exemplo, 123',
-    telefone: '(11) 1234-5678',
-    professores: 20,
-    rede: 'Pública',
-    etapasdeensino: 'Fundamental',
-    numero: '123',
-    cep: '12345-678',
-  };
+  const fetchEscolaSelecionada = async () => {
+    const escolas = await fetchData(escolaId);
+    setEscolaSelecionada(escolas);
+  }
+  useEffect(()=>{
+    fetchEscolaSelecionada()
+  }, []);
   
   return (
       <Modal className={"default"} closeModal={() => onClose()}>
-          
-          <h4 className="text-center mt-2">{escolaSelecionada?.escola}</h4>
           
           <div className="modal-content">
               <h2 style={{ fontSize: '16px' }}><strong>Detalhes da Escola</strong></h2>
               {escolaSelecionada && (
                   <div>
-                      <p style={{ fontSize: '12px' }}>Nome: {escolaSelecionada.escola}</p>
-                      <p style={{ fontSize: '12px' }}>Posição: {escolaSelecionada.posicao}</p>
+                      <p style={{ fontSize: '12px' }}>Nome: {escolaSelecionada.escola.nomeEscola}</p>
+                      <p style={{ fontSize: '12px' }}>Posição: {escolaSelecionada.ranqueInfo.posicao}</p>
                       <p style={{ fontSize: '12px' }}>Pontuação:</p>
                       Fator X, Peso X, Valor X<br/>
                       Fator Y, Peso Y, Valor Y<br/>
                       Fator Z, Peso Z, Valor Z<br/>
                       UPS, Peso 2,16<br/>
-                      <p style={{ fontSize: '12px' }}>Total: {escolaSelecionada.pontuacao}</p>
+                      <p style={{ fontSize: '12px' }}>Total: {escolaSelecionada.ranqueInfo.pontuacao}</p>
                       <p style={{ fontSize: '12px' }}><strong>Dados</strong></p>
-                      <p style={{ fontSize: '12px' }}>Código: {escolaSelecionada.código}</p>
-                      <p style={{ fontSize: '12px' }}>Alunos: {escolaSelecionada.alunos}</p>
-                      <p style={{ fontSize: '12px' }}>Porte: {escolaSelecionada.porte}</p>
-                      <p style={{ fontSize: '12px' }}>Situação: {escolaSelecionada.situação}</p>
+                      <p style={{ fontSize: '12px' }}>Código: {escolaSelecionada.escola.codigoEscola}</p>
+                      <p style={{ fontSize: '12px' }}>Alunos: {escolaSelecionada.escola.numeroTotalDeAlunos}</p>
+                      <p style={{ fontSize: '12px' }}>Porte: {escolaSelecionada.escola.porte}</p>
+                      <p style={{ fontSize: '12px' }}>Situação: {escolaSelecionada.escola.situacao}</p>
                   </div>
               )}
 
@@ -76,14 +50,15 @@ const ModalRanqueEscola: React.FC<ModalProps> = ({ isOpen, onClose, onCreateAcao
                   {escolaSelecionada && (
                       <div>
                           <p style={{ fontSize: '12px' }}><strong>Endereço</strong></p>
-                          {escolaSelecionada.endereço}
-                          <p style={{ fontSize: '12px' }}>Telefone: {escolaSelecionada.telefone}</p>
-                          <p style={{ fontSize: '12px' }}>Professores: {escolaSelecionada.professores}</p>
-                          <p style={{ fontSize: '12px' }}>Rede: {escolaSelecionada.rede}</p>
-                          <p style={{ fontSize: '12px' }}>Etapas de Ensino: {escolaSelecionada.etapasdeensino}</p>
-                          <p style={{ fontSize: '12px' }}>Número: {escolaSelecionada.numero}</p>
-                          <p style={{ fontSize: '12px' }}>Cep: {escolaSelecionada.cep}</p>
-                          <p style={{ fontSize: '12px' }}>Estado: {escolaSelecionada.uf}</p>
+                          {escolaSelecionada.escola.endereco}
+                          <p style={{ fontSize: '12px' }}>Telefone: {escolaSelecionada.escola.telefone}</p>
+                          <p style={{ fontSize: '12px' }}>Professores: {escolaSelecionada.escola.numeroTotalDeDocentes}</p> 
+                          <p style={{ fontSize: '12px' }}>Rede: {escolaSelecionada.escola.rede}</p>
+                          <p style={{ fontSize: '12px' }}>Etapas de Ensino: {escolaSelecionada.escola.etapasEnsino}</p>
+                          {/*sem numero*/}
+                          <p style={{ fontSize: '12px' }}>Número: {escolaSelecionada.escola.telefone}</p> 
+                          <p style={{ fontSize: '12px' }}>Cep: {escolaSelecionada.escola.cep}</p>
+                          <p style={{ fontSize: '12px' }}>Estado: {escolaSelecionada.escola.uf}</p>
                       </div>
                   )} 
               </div>
