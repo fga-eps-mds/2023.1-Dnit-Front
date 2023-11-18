90588a93b14f3eabd55fb7be9ad6a738ed300b5d
Artur Bartz <arturbylly2@gmail.com>
2023-11-18 15:01:31 -0300

    feat: adiciona o onClose na chamada do modal do src/components/EditarTipoPerfilDialog
    Co-authored-by: Eduardo Sandes <eduardo.sandes6@gmail.com>

--- src/components/EditarTipoPerfilDialog/index.tsx
+++ src/components/EditarTipoPerfilDialog/index.tsx
@@ -66,7 +66,7 @@ export function EditarTipoPerfilDialog({ closeDialog, listaOpcoes, usuarioId, li
   if (loading) {
     
     return (
-      <Modal className="modal-title">
+      <Modal className="modal-title" closeModal={() => closeDialog(false)}>
         <h4 className="text-center mt-2">Carregando Edição de Perfil... </h4>
         <div className="d-flex justify-content-center m-4">
           <ReactLoading type="spinningBubbles" color="#000000" />
@@ -77,7 +77,7 @@ export function EditarTipoPerfilDialog({ closeDialog, listaOpcoes, usuarioId, li
   }
 
   return (
-    <Modal className="modal-title ">
+    <Modal className="modal-title " closeModal={() => closeDialog(false)}>
       {contextHolder}
       <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
         <h4 className="text-center mt-1">Tipo Perfil</h4>
