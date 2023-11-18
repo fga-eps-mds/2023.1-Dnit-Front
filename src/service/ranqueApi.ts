aeea82199a64da4f919ee3d6f354239aa008288f
Artur Bartz <arturbylly2@gmail.com>
2023-11-18 16:04:35 -0300

    feat: adiciona os export das funções de fetch dos dados de emProcessamento e DataFim do banco do back para serem importados na pagina de ranqueamento das escolas
    Co-authored-by: Eduardo Sandes <eduardo.sandes6@gmail.com>
    Co-authored-by: Thiago Paiva <thiagohdaqw@gmail.com>

--- src/service/ranqueApi.ts
+++ src/service/ranqueApi.ts
@@ -6,3 +6,28 @@ import { EscolaRanqueData, EscolaRanqueFiltro, ListaPaginada } from "../models/r
 export async function fetchEscolasRanque(filtro: EscolaRanqueFiltro): Promise<ListaPaginada<EscolaRanqueData>>{
     return fetchDados<ListaPaginada<EscolaRanqueData>>(URL.listarEscolasRanque, filtro);
 }
+
+export async function fetchProcessamentoRanque() {
+    try {
+      const response = await fetch('api/ranque/processamento');
+      const data = await response.json();
+      const emProcesso = data.emProgresso;
+      return emProcesso;
+    } catch (error) {
+      console.error('Erro ao buscar dados de processamento:', error);
+      throw error;
+    }
+  }
+  
+  export async function fetchProcessamentoDataRanque() {
+    try {
+      const response = await fetch('api/ranque/processamento');
+      const data = await response.json();
+      const dataFim = data.dataFim;
+      return dataFim;
+    } catch (error) {
+      console.error('Erro ao buscar dados de processamento:', error);
+      throw error;
+    }
+  }
+  
